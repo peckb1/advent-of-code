7c99348f1832d248938381553e52ad2355db0efb
Brian Peck <peckb@squareup.com>
2021-12-01 17:31:02 -0800

    constants

--- src/test/kotlin/me/peckb/aoc/_2021/Day1Test.kt
+++ src/test/kotlin/me/peckb/aoc/_2021/Day1Test.kt
@@ -7,6 +7,11 @@ import org.junit.jupiter.api.Test
 import javax.inject.Inject
 
 internal class Day1Test {
+  companion object {
+    const val DAY_1_PART_1 = "src/test/resources/2021/day1.first.input"
+    const val DAY_1_PART_2 = "src/test/resources/2021/day1.second.input"
+  }
+
   @BeforeEach
   fun setup() {
     DaggerTestDayComponent.create().inject(this)
@@ -16,11 +21,11 @@ internal class Day1Test {
 
   @Test
   fun testDay1PartOne() {
-    assertEquals(1390, day1.largerMeasurements("src/test/resources/2021/day1.first.input"))
+    assertEquals(1390, day1.largerMeasurements(DAY_1_PART_1))
   }
 
   @Test
   fun testDay1PartTwo() {
-    assertEquals(1457, day1.largerGroupedMeasurements("src/test/resources/2021/day1.second.input"))
+    assertEquals(1457, day1.largerGroupedMeasurements(DAY_1_PART_2))
   }
 }
