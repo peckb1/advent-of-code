ef6e01dceadd9f7cf704bef53eb630c2296ad354
Brian Peck <peckb@squareup.com>
2021-12-02 17:42:34 -0800

    Quick Renaming for consistency

--- src/main/kotlin/me/peckb/aoc/_2021/InputModule.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/InputModule.kt
@@ -3,15 +3,15 @@ package me.peckb.aoc._2021
 import dagger.Binds
 import dagger.Module
 import me.peckb.aoc._2021.generators.InputGenerator
-import me.peckb.aoc._2021.generators.IntInputGenerator
+import me.peckb.aoc._2021.generators.IntGenerator
 import me.peckb.aoc._2021.generators.Path
 import me.peckb.aoc._2021.generators.PathGenerator
 
 @Module
 internal abstract class InputModule {
   @Binds
-  abstract fun intInputGenerator(intInputGenerator: IntInputGenerator): InputGenerator<Int>
+  abstract fun intInputGenerator(intGenerator: IntGenerator): InputGenerator<Int>
 
   @Binds
-  abstract fun pathInputGenerator(intInputGenerator: PathGenerator): InputGenerator<Path>
+  abstract fun pathInputGenerator(pathGenerator: PathGenerator): InputGenerator<Path>
 }
--- src/main/kotlin/me/peckb/aoc/_2021/generators/IntGenerator.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/generators/IntGenerator.kt
@@ -3,7 +3,7 @@ package me.peckb.aoc._2021.generators
 import java.io.File
 import javax.inject.Inject
 
-class IntInputGenerator @Inject constructor() : InputGenerator<Int>() {
+class IntGenerator @Inject constructor() : InputGenerator<Int>() {
   override fun <Out> usingInput(filename: String, sequenceHandler: (Sequence<Int>) -> Out): Out =
     sequenceHandler(File(filename).toLineSequence { it.toInt() })
 }
