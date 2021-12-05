07c061d98027467fd27c8cb91267b9c69ef4eddd
Brian Peck <peckb@squareup.com>
2021-12-04 22:19:53 -0800

    Day 05 2021 (#4)
    * Day 05 2021

---
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day05/Day05.kt
@@ -0,0 +1,81 @@
+package me.peckb.aoc._2021.calendar.day05
+
+import me.peckb.aoc._2021.generators.InputGenerator.InputGeneratorFactory
+import javax.inject.Inject
+import kotlin.ranges.IntProgression.Companion.fromClosedRange
+
+data class Point(val x : Int, val y: Int)
+
+typealias Line = Pair<Point, Point>
+typealias Map = Array<Array<Int>>
+
+class Day05 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
+  companion object {
+    private const val MAP_SIZE = 1000
+    private fun createEmptyMap() = (0 until MAP_SIZE).map { Array(MAP_SIZE) { 0 } }.toTypedArray()
+  }
+
+  fun nonDiagonalOverlapCount(fileName: String) = generatorFactory.forFile(fileName).readAs(::line) { input ->
+    val map = createEmptyMap()
+
+    input.filter { it.isVerticalOrHorizontal() }
+      .forEach { line -> adjustAreaWithNonDiagonal(map, line) }
+
+    map.overlap()
+  }
+
+  fun fullOverlapCount(fileName: String) = generatorFactory.forFile(fileName).readAs(::line) { input ->
+    val map = createEmptyMap()
+
+    input.forEach { line ->
+      if (line.isVerticalOrHorizontal()) {
+        adjustAreaWithNonDiagonal(map, line)
+      } else {
+        adjustAreaWithDiagonal(map, line)
+      }
+    }
+
+    map.overlap()
+  }
+
+  private fun adjustAreaWithNonDiagonal(area: Map, line: Pair<Point, Point>) {
+    val sortedX = line.sortedX()
+    val sortedY = line.sortedY()
+
+    sortedX.forEach { x ->
+      sortedY.forEach { y ->
+        area[y][x]++
+      }
+    }
+  }
+
+  private fun adjustAreaWithDiagonal(area: Map, line: Line) {
+    val xStep = line.second.x.compareTo(line.first.x)
+    val xProgression = fromClosedRange(line.first.x, line.second.x, xStep)
+
+    val yStep = line.second.y.compareTo(line.first.y)
+    val yProgression = fromClosedRange(line.first.y, line.second.y, yStep)
+
+    xProgression.zip(yProgression).forEach { (x, y) ->
+      area[y][x]++
+    }
+  }
+
+  private fun line(line: String): Line = line.split("->").let { coordinates ->
+    val points = coordinates.flatMap { coordinateString ->
+      coordinateString.trim()
+        .split(",")
+        .map { it.toInt() }
+        .zipWithNext { x, y -> Point(x, y) }
+    }
+    points.first() to points.last()
+  }
+
+  private fun Line.isVerticalOrHorizontal() = first.x == second.x || first.y == second.y
+
+  private fun Line.sortedX() = listOf(first.x, second.x).sorted().let { it.first() .. it.last() }
+
+  private fun Line.sortedY() = listOf(first.y, second.y).sorted().let { it.first() .. it.last() }
+
+  private fun Map.overlap() = sumOf { row -> row.count { it >= 2 } }
+}
---
+++ src/test/kotlin/me/peckb/aoc/_2021/calendar/day05/Day05Test.kt
@@ -0,0 +1,30 @@
+package me.peckb.aoc._2021.calendar.day05
+
+import me.peckb.aoc._2021.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+internal class Day05Test {
+  companion object {
+    const val DAY_05 = "src/test/resources/2021/day05.input"
+  }
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Inject internal lateinit var day05: Day05
+
+  @Test
+  fun testDay05PartOne() {
+    assertEquals(5585, day05.nonDiagonalOverlapCount(DAY_05))
+  }
+
+  @Test
+  fun testDay05PartTwo() {
+    assertEquals(17193, day05.fullOverlapCount(DAY_05))
+  }
+}
