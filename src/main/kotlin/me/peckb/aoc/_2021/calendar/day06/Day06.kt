de20374b01cda943c9b71523fa9770efe675b6e5
Brian Peck <peckb@squareup.com>
2021-12-06 00:16:29 -0800

    code cleanup and update the README

--- src/main/kotlin/me/peckb/aoc/_2021/calendar/day06/Day06.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day06/Day06.kt
@@ -1,6 +1,8 @@
 package me.peckb.aoc._2021.calendar.day06
 
 import me.peckb.aoc._2021.generators.InputGenerator
+import java.time.DayOfWeek
+import java.time.DayOfWeek.*
 import javax.inject.Inject
 
 class Day06 @Inject constructor(private val generatorFactory: InputGenerator.InputGeneratorFactory) {
@@ -9,6 +11,7 @@ class Day06 @Inject constructor(private val generatorFactory: InputGenerator.Inp
     const val PART_TWO_OBSERVATION_PERIOD = 256
   }
 
+  /** BRUTE FORCE */
   fun partOne(fileName: String) = generatorFactory.forFile(fileName).read { input ->
     val population = mutableListOf<Fish>()
     input.first().split(",").forEach { age ->
@@ -26,47 +29,39 @@ class Day06 @Inject constructor(private val generatorFactory: InputGenerator.Inp
     population.size
   }
 
-
+  /** COUNT THE BIRTH DAYS! */
   fun partTwo(fileName: String) = generatorFactory.forFile(fileName).read { input ->
-    val population = mutableListOf<Fish>()
-    input.first().split(",").forEach { age ->
-      population.add(Fish(age.toInt()))
-    }
-
-    val parentsGivingBirth: MutableMap<Int, Pair<Long, Long>> = mutableMapOf(
-      0 to (0L to 0L),
-      1 to (0L to 0L),
-      2 to (0L to 0L),
-      3 to (0L to 0L),
-      4 to (0L to 0L),
-      5 to (0L to 0L),
-      6 to (0L to 0L)
+    val birthDays: MutableMap<DayOfWeek, BirthWindow> = mutableMapOf(
+      MONDAY to BirthWindow(),
+      TUESDAY to BirthWindow(),
+      WEDNESDAY to BirthWindow(),
+      THURSDAY to BirthWindow(),
+      FRIDAY to BirthWindow(),
+      SATURDAY to BirthWindow(),
+      SUNDAY to BirthWindow()
     )
 
-    population.map { fish ->
-      val myChildDay = fish.myAge()
-      parentsGivingBirth[myChildDay] = (parentsGivingBirth[myChildDay]!!.first + 1) to 0L
+    val population = input.first().split(",").map { Fish(it.toInt()) }.apply {
+      this.map { birthDays[of(it.myAge() + 1)]?.addBirth() }
     }
 
-    var today = -1
+    var today = SUNDAY
     var total: Long = population.size.toLong()
-    repeat(PART_TWO_OBSERVATION_PERIOD) { day ->
-      (today++)
-      today %= 7
-      val tomorrow = (today + 9) % 7 // the "counter" going to 8 starting the next day - means we need to jump two days ahead, next week
-      val parentsGivingBirthToday = parentsGivingBirth[today]!!
-      if (parentsGivingBirthToday.first > 0) {
-        // a parent is giving birth today, add a child
-        total += parentsGivingBirthToday.first
-        // if we have any waiting children for "next week" add them to today
-        // and then reset those "waiting" for next week
-        parentsGivingBirth[today] = parentsGivingBirthToday.first + parentsGivingBirth[today]!!.second to 0
-        // add today's count to tomorrows "waiting"
-        parentsGivingBirth[tomorrow] = parentsGivingBirth[tomorrow]!!.first to parentsGivingBirthToday.first
-      } else {
-        // no one is giving birth today, so just move our waiting to available next week
-        parentsGivingBirth[today] = parentsGivingBirth[today]!!.first + parentsGivingBirth[today]!!.second to 0
+
+    repeat(PART_TWO_OBSERVATION_PERIOD) {
+      today = today.plus(1)
+      val todaysBirths = birthDays[today]!!.todaysBirths
+
+      if (todaysBirths > 0) {
+        // a parent is giving birth today, add the children to our count
+        total += todaysBirths
+        // add today's count to next birth windows "waiting"
+        // DEV NOTE: the counter needs to be "8" tomorrow, so we add "9" today
+        birthDays[today.plus(9)]?.setNextWeeksBirth(todaysBirths)
       }
+      // if we have any waiting children for "next week" add them to today
+      // and then reset those "waiting" for next week
+      birthDays[today]?.moveNextWeekToToday()
     }
 
     total
@@ -79,5 +74,20 @@ class Day06 @Inject constructor(private val generatorFactory: InputGenerator.Inp
     }
 
     fun myAge() = daysUntilSpawn
+  }
+
+  data class BirthWindow(var todaysBirths: Long = 0, private var nextWeeksBirths: Long = 0) {
+    fun addBirth() {
+      todaysBirths++
+    }
+
+    fun setNextWeeksBirth(birthCount: Long) {
+      nextWeeksBirths = birthCount
+    }
+
+    fun moveNextWeekToToday() {
+      todaysBirths += nextWeeksBirths
+      nextWeeksBirths = 0
+    }
   }
 }
