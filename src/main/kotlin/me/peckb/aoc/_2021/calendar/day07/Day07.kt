fa75bfa2918ceb5f76cc784c1955f155938f61ec
Brian Peck <peckb@squareup.com>
2021-12-06 21:40:41 -0800

    day07 2021 (#6)
    * Day07 2021

---
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day07/Day07.kt
@@ -0,0 +1,42 @@
+package me.peckb.aoc._2021.calendar.day07
+
+import me.peckb.aoc._2021.generators.InputGenerator
+import javax.inject.Inject
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+class Day07 @Inject constructor(private val generatorFactory: InputGenerator.InputGeneratorFactory) {
+  fun findSimpleCrabCost(fileName: String) = generatorFactory.forFile(fileName).read { input ->
+    findMinCrabCost(input.first()) { (crabPosition, desiredPosition) ->
+      abs(desiredPosition - crabPosition)
+    }
+  }
+
+  fun findComplexCrabCost(fileName: String) = generatorFactory.forFile(fileName).read { input ->
+    findMinCrabCost(input.first()) { (crabPosition, desiredPosition) ->
+      (1 .. abs(desiredPosition - crabPosition)).sum()
+    }
+  }
+
+  private fun findMinCrabCost(movementStrings: String, movementCost: (Movement) -> Int): Int {
+    var min = Int.MAX_VALUE
+    var max = Int.MIN_VALUE
+    val horizontalPositions = movementStrings.split(",").map {
+      it.toInt().also { position ->
+        min = min(position, min)
+        max = max(position, max)
+      }
+    }
+
+    var cost = Int.MAX_VALUE
+    (min .. max).forEach lit@{ desiredPosition ->
+      val posCost = horizontalPositions.sumOf { movementCost(Movement(it, desiredPosition)) }
+      cost = min(cost, posCost)
+    }
+
+    return cost
+  }
+
+  private data class Movement(val crabPosition: Int, val desiredPosition: Int)
+}
---
+++ src/test/kotlin/me/peckb/aoc/_2021/calendar/day07/Day07Test.kt
@@ -0,0 +1,30 @@
+package me.peckb.aoc._2021.calendar.day07
+
+import me.peckb.aoc._2021.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+internal class Day07Test {
+  companion object {
+    const val DAY_07 = "src/test/resources/2021/day07.input"
+  }
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Inject internal lateinit var day07: Day07
+
+  @Test
+  fun testDay05PartOne() {
+    assertEquals(355150, day07.findSimpleCrabCost(DAY_07))
+  }
+
+  @Test
+  fun testDay05PartTwo() {
+    assertEquals(98368490, day07.findComplexCrabCost(DAY_07))
+  }
+}
