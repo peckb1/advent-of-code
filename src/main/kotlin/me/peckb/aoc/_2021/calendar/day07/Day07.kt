b87c7406d8d660f8a2f2664aed993185be5714ad
Brian Peck <peckb@squareup.com>
2021-12-06 22:07:08 -0800

    Use the actual Trianlge number formula

--- src/main/kotlin/me/peckb/aoc/_2021/calendar/day07/Day07.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day07/Day07.kt
@@ -2,41 +2,31 @@ package me.peckb.aoc._2021.calendar.day07
 
 import me.peckb.aoc._2021.generators.InputGenerator
 import javax.inject.Inject
+import kotlin.Int.Companion.MAX_VALUE
+import kotlin.Int.Companion.MIN_VALUE
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
 class Day07 @Inject constructor(private val generatorFactory: InputGenerator.InputGeneratorFactory) {
   fun findSimpleCrabCost(fileName: String) = generatorFactory.forFile(fileName).readOne { data ->
-    findMinCrabCost(data) { (crabPosition, desiredPosition) ->
-      abs(desiredPosition - crabPosition)
-    }
+    findMinCrabCost(data) { it }
   }
 
   fun findComplexCrabCost(fileName: String) = generatorFactory.forFile(fileName).readOne { data ->
-    findMinCrabCost(data) { (crabPosition, desiredPosition) ->
-      (1 .. abs(desiredPosition - crabPosition)).sum()
+    findMinCrabCost(data) { triangleNumber(it) }
+  }
+
+  private fun findMinCrabCost(movementStrings: String, movementCost: (Int) -> Int): Int {
+    val horizontalPositions = movementStrings.split(",").map { it.toInt() }
+    val minPosition = horizontalPositions.minOf { it }
+    val maxPosition = horizontalPositions.maxOf { it }
+
+    return (minPosition .. maxPosition).fold(MAX_VALUE) { min, desiredPosition ->
+      val posCost = horizontalPositions.sumOf { movementCost(abs(desiredPosition - it)) }
+      min(min, posCost)
     }
   }
 
-  private fun findMinCrabCost(movementStrings: String, movementCost: (Movement) -> Int): Int {
-    var min = Int.MAX_VALUE
-    var max = Int.MIN_VALUE
-    val horizontalPositions = movementStrings.split(",").map {
-      it.toInt().also { position ->
-        min = min(position, min)
-        max = max(position, max)
-      }
-    }
-
-    var cost = Int.MAX_VALUE
-    (min .. max).forEach lit@{ desiredPosition ->
-      val posCost = horizontalPositions.sumOf { movementCost(Movement(it, desiredPosition)) }
-      cost = min(cost, posCost)
-    }
-
-    return cost
-  }
-
-  private data class Movement(val crabPosition: Int, val desiredPosition: Int)
+  private fun triangleNumber(n: Int) = ((n + 1) * n) / 2
 }
