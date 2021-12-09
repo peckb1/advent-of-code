76490aedeabf6a0a11d4089747dfb24fd3f7d39b
Brian Peck <peckb@squareup.com>
2021-12-08 22:44:56 -0800

    Day09 2021 (#8)
    * Day 09 2021

---
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day09/Day09.kt
@@ -0,0 +1,84 @@
+package me.peckb.aoc._2021.calendar.day09
+
+import me.peckb.aoc._2021.generators.InputGenerator.InputGeneratorFactory
+import javax.inject.Inject
+import kotlin.Int.Companion.MAX_VALUE
+
+typealias Topography = List<List<Int>>
+
+class Day09 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
+  fun findLowPointSum(fileName: String) = generatorFactory.forFile(fileName).readAs(::heights) { input ->
+    input.toList().findLowPoints().sumOf { it.height + 1 }
+  }
+
+  fun findLargestBasinProduct(fileName: String) = generatorFactory.forFile(fileName).readAs(::heights) { input ->
+    val floor = input.toList()
+    val minLocations = floor.findLowPoints()
+
+    val basins = minLocations.map { (value, y, x) ->
+      val initialLocation = Location(value, y, x)
+
+      val spacesInBasin = mutableSetOf(initialLocation)
+      val spacesToExplore = mutableSetOf<Location>().apply {
+        addAll(initialLocation.generateExploringNeighbors())
+      }
+
+      while(spacesToExplore.isNotEmpty()) {
+        val spaceToExplore = spacesToExplore.first().also { spacesToExplore.remove(it) }
+        val valueOfMe = floor.findHeight(spaceToExplore.y, spaceToExplore.x)
+
+        if (valueOfMe in (spaceToExplore.height + 1)..8) {
+          val exploredLocation = Location(valueOfMe, spaceToExplore.y, spaceToExplore.x)
+
+          spacesInBasin.add(exploredLocation)
+          spacesToExplore.addAll(exploredLocation.generateExploringNeighbors())
+        }
+      }
+
+      spacesInBasin
+    }
+
+    basins.sortedByDescending { it.size }
+      .take(3)
+      .map { it.size }
+      .reduce(Int::times)
+  }
+
+  private fun heights(line: String) = line.map(Character::getNumericValue)
+
+  private fun Topography.findLowPoints(): List<Location> {
+    val minLocations = mutableListOf<Location>()
+
+    repeat(this[0].size) { x ->
+      repeat(this.size) { y ->
+        val me = this[y][x]
+
+        val up = findHeight(y - 1, x)
+        val down = findHeight(y + 1, x)
+        val left = findHeight(y, x - 1)
+        val right = findHeight(y, x + 1)
+
+        if (me < up && me < down && me < left && me < right) {
+          minLocations.add(Location(me, y, x))
+        }
+      }
+    }
+
+    return minLocations
+  }
+
+  private fun Topography.findHeight(y: Int, x: Int) = try {
+    this[y][x]
+  } catch(e: IndexOutOfBoundsException) {
+    MAX_VALUE
+  }
+
+  private data class Location(val height: Int, val y: Int, val x: Int) {
+    fun generateExploringNeighbors() = setOf(
+      Location(height, y - 1, x),
+      Location(height, y + 1, x),
+      Location(height, y, x - 1),
+      Location(height, y, x + 1)
+    )
+  }
+}
---
+++ src/test/kotlin/me/peckb/aoc/_2021/calendar/day09/Day09Test.kt
@@ -0,0 +1,30 @@
+package me.peckb.aoc._2021.calendar.day09
+
+import me.peckb.aoc._2021.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+internal class Day09Test {
+  companion object {
+    const val DAY_09 = "src/test/resources/2021/day09.input"
+  }
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Inject internal lateinit var day09: Day09
+
+  @Test
+  fun testDay09PartOne() {
+    assertEquals(504, day09.findLowPointSum(DAY_09))
+  }
+
+  @Test
+  fun testDay09PartTwo() {
+    assertEquals(1558722, day09.findLargestBasinProduct(DAY_09))
+  }
+}
