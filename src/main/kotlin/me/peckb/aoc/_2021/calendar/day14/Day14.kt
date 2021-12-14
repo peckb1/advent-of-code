07cc7058e9a464fe6e40ca70f1684f084610f122
Brian Peck <peckb@squareup.com>
2021-12-13 23:17:16 -0800

    final refactor before readme

--- src/main/kotlin/me/peckb/aoc/_2021/calendar/day14/Day14.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day14/Day14.kt
@@ -13,27 +13,9 @@ class Day14 @Inject constructor(private val generatorFactory: InputGeneratorFact
   }
 
   private fun findCounts(data: List<String>, iterations: Int): Map<Char, Long> {
-    // { PP=2, PF=2, FC=1, CH=1, HP=1, FN=1, NC=1, CK=1, KO=2, OK=1, OS=1, SB=1, BV=1, VC=1, CF=1, FP=1 }
-    val characterPairs: MutableMap<String, Long> = data.first()
-      .windowed(2)
-      .groupBy { it }
-      .mapValues { (_, pairing) -> pairing.size.toLong() }
-      .toMutableMap()
-
-    // [VC->N, SC->H, ..., CH->K, NH->P]
-    val instructions: List<Instruction> = data.drop(2).map {
-      val (pattern, insertion) = it.split(" -> ")
-      Instruction(pattern, insertion)
-    }
-
-    // { P=5, F=3, C=3, H=1, N=1, K=2, O=2, S=1, B=1, V=1 }
-    val counts: MutableMap<Char, Long> = mutableMapOf<Char, Long>().apply {
-      data.first().forEach {
-        this.compute(it) { _, v ->
-          (v ?: 0) + 1
-        }
-      }
-    }
+    val characterPairs: MutableMap<String, Long> = createInitialPairCounts(data.first())
+    val instructions: List<Instruction> = createInstructions(data.drop(2))
+    val counts: MutableMap<Char, Long> = createInitialCounts(data.first())
 
     repeat(iterations) {
       val addActions = mutableListOf<Pair<String, Long>>()
@@ -55,6 +37,7 @@ class Day14 @Inject constructor(private val generatorFactory: InputGeneratorFact
           counts.compute(insert.first()) { _, existingCount -> (existingCount ?: 0) + c }
         }
       }
+
       addActions.forEach { (pattern, count) ->
         characterPairs.compute(pattern) { _, maybeCount -> (maybeCount ?: 0) + count }
       }
@@ -68,6 +51,38 @@ class Day14 @Inject constructor(private val generatorFactory: InputGeneratorFact
 
     return counts
   }
+
+  /**
+   * {
+   *   PP=2, PF=2, FC=1, CH=1, HP=1, FN=1,
+   *   NC=1, CK=1, KO=2, OK=1, OS=1, SB=1,
+   *   BV=1, VC=1, CF=1, FP=1
+   * }
+   */
+  private fun createInitialPairCounts(data: String) = data
+    .windowed(2)
+    .groupBy { it }
+    .mapValues { (_, pairing) -> pairing.size.toLong() }
+    .toMutableMap()
+
+  /**
+   * [
+   *   VC->N, SC->H, ..., CH->K, NH->P
+   * ]
+   */
+  private fun createInstructions(instructions: List<String>) = instructions
+    .map {
+      val (pattern, insertion) = it.split(" -> ")
+      Instruction(pattern, insertion)
+    }
+
+  /**
+   * {
+   *   P=5, F=3, C=3, H=1, N=1, K=2, O=2, S=1, B=1, V=1
+   * }
+   */
+  private fun createInitialCounts(data: String) = mutableMapOf<Char, Long>().apply {
+    data.forEach { compute(it) { _, v -> (v ?: 0) + 1 } } }
 
   private fun countHighMinusLow(counts: Map<Char, Long>): Long {
     val sortedPairs = counts.entries.sortedByDescending { it.value }
