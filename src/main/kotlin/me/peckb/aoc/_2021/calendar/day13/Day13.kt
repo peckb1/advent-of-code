f89d57e845a0a2903c47c6f90366452b36da2e20
Brian Peck <peckb@squareup.com>
2021-12-13 18:22:05 -0800

    swap from char to boolean for comparisons

--- src/main/kotlin/me/peckb/aoc/_2021/calendar/day13/Day13.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day13/Day13.kt
@@ -7,15 +7,14 @@ import kotlin.math.max
 
 class Day13 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
   companion object {
-    const val ACTIVE = '#'
-    const val INACTIVE = ' '
+    const val ACTIVE = true
+    const val INACTIVE = false
     const val FOLD_PREFIX = "fold along "
   }
 
   fun oneFoldCount(fileName: String) = generatorFactory.forFile(fileName).read { input ->
     val (dotLines, foldInstructions) = parseInput(input)
-    val (dots, maxX, maxY) = generateDots(dotLines)
-    val paper = generatePaper(dots, maxX, maxY)
+    val paper = fillPaper(generatePaperData(dotLines))
 
     val (updatedX, updatedY) = foldPaper(paper, foldInstructions.take(1))
 
@@ -28,24 +27,23 @@ class Day13 @Inject constructor(private val generatorFactory: InputGeneratorFact
 
   fun everyFoldCode(fileName: String) = generatorFactory.forFile(fileName).read { input ->
     val (dotLines, foldInstructions) = parseInput(input)
-    val (dots, maxX, maxY) = generateDots(dotLines)
-    val paper = generatePaper(dots, maxX, maxY)
+    val paper = fillPaper(generatePaperData(dotLines))
 
     val (updatedX, updatedY) = foldPaper(paper, foldInstructions)
 
-    val codes = Array(updatedY + 1) { Array(updatedX + 1) { INACTIVE } }
+    val codes = Array(updatedY + 1) { Array(updatedX + 1) { ' ' } }
     (0 until updatedY).forEach { y ->
       (0 until updatedX).forEach { x ->
-        codes[y][x] = paper[y][x]
+        codes[y][x] = if (paper[y][x]) '#' else ' '
       }
     }
 
     codes.joinToString("\n") { it.joinToString("") }
   }
 
-  private fun generatePaper(dots: List<Dot>, maxX: Int, maxY: Int) =
-    Array(maxY + 1) { Array(maxX + 1) { INACTIVE } }.apply {
-      dots.forEach { (x, y) -> this[y][x] = ACTIVE }
+  private fun fillPaper(paper: Paper) =
+    Array(paper.y + 1) { Array(paper.x + 1) { INACTIVE } }.apply {
+      paper.dots.forEach { (x, y) -> this[y][x] = ACTIVE }
     }
 
   private fun parseInput(input: Sequence<String>): Pair<List<String>, List<String>> {
@@ -63,18 +61,12 @@ class Day13 @Inject constructor(private val generatorFactory: InputGeneratorFact
     return dotLines to foldInstructions
   }
 
-  private fun foldPaper(paper: Array<Array<Char>>, foldInstructions: List<String>): Pair<Int, Int> {
+  private fun foldPaper(paper: Array<Array<Boolean>>, foldInstructions: List<String>): Pair<Int, Int> {
     var maxX = paper[0].size
     var maxY = paper.size
 
     fun fold(sourceX: Int, sourceY: Int, destinationX: Int, destinationY: Int) {
-      val source = paper[sourceY][sourceX]
-
-      paper[destinationY][destinationX] = if (paper[destinationY][destinationX] == ACTIVE || source == ACTIVE) {
-        ACTIVE
-      } else {
-        INACTIVE
-      }
+      if (paper[sourceY][sourceX] == ACTIVE) paper[destinationY][destinationX] = ACTIVE
     }
 
     foldInstructions.forEach {
@@ -100,12 +92,12 @@ class Day13 @Inject constructor(private val generatorFactory: InputGeneratorFact
     return maxX to maxY
   }
 
-  private fun generateDots(dotLines: List<String>): Triple<List<Dot>, Int, Int>{
+  private fun generatePaperData(dotLines: List<String>): Paper {
     var maxX = MIN_VALUE
     var maxY = MIN_VALUE
 
-    val dots = dotLines.map {
-      it.split(",").map{ it.toInt() }.let { (x, y) ->
+    val dots = dotLines.map { line ->
+      line.split(",").map{ it.toInt() }.let { (x, y) ->
         maxX = max(maxX, x)
         maxY = max(maxY, y)
 
@@ -113,8 +105,10 @@ class Day13 @Inject constructor(private val generatorFactory: InputGeneratorFact
       }
     }
 
-    return Triple(dots, maxX, maxY)
+    return Paper(dots, maxX, maxY)
   }
 
   data class Dot(val x: Int, val y: Int)
+
+  data class Paper(val dots: List<Dot>, val x: Int, val y: Int)
 }
