1711571274aada9b358aa8377dea414506052e52
Brian Peck <peckb@squareup.com>
2021-12-16 20:30:31 -0800

    a completely unecessary rearangement

--- src/main/kotlin/me/peckb/aoc/_2021/calendar/day16/Day16.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day16/Day16.kt
@@ -9,10 +9,10 @@ import javax.inject.Inject
 class Day16 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
   companion object {
     val CONVERSIONS = mapOf(
-      '0' to "0000", '1' to "0001", '2' to "0010", '3' to "0011",
-      '4' to "0100", '5' to "0101", '6' to "0110", '7' to "0111",
-      '8' to "1000", '9' to "1001", 'A' to "1010", 'B' to "1011",
-      'C' to "1100", 'D' to "1101", 'E' to "1110", 'F' to "1111"
+      '0' to "0000", '4' to "0100", '8' to "1000", 'C' to "1100",
+      '1' to "0001", '5' to "0101", '9' to "1001", 'D' to "1101",
+      '2' to "0010", '6' to "0110", 'A' to "1010", 'E' to "1110",
+      '3' to "0011", '7' to "0111", 'B' to "1011", 'F' to "1111"
     )
   }
 
