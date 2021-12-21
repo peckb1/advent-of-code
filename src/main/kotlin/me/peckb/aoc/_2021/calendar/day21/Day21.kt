6b11fb4c3cdc41a75e1db55e9da2ff364e1fc8be
Brian Peck <peckb@squareup.com>
2021-12-21 00:09:29 -0800

    Day 21 2021 Skeleton (#21)
    * Day 21 2021

---
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day21/Day21.kt
@@ -0,0 +1,106 @@
+package me.peckb.aoc._2021.calendar.day21
+
+import me.peckb.aoc._2021.generators.InputGenerator.InputGeneratorFactory
+import javax.inject.Inject
+
+class Day21 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
+  companion object {
+    val BOARD = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  }
+
+  fun partOne(fileName: String) = generatorFactory.forFile(fileName).readAs(::player) { input ->
+    val players = input.toList()
+    val die = DeterministicD100()
+
+    var playerCounter = 0
+    while(players.none { it.points >= 1000 }) {
+      val player = players[playerCounter % players.size]
+      val distanceToTravel = (1..3).sumOf { die.roll() }
+      val circularDistance = distanceToTravel % BOARD.size
+      player.zeroIndexedSpace += circularDistance
+      player.zeroIndexedSpace %= BOARD.size
+      player.points+= BOARD[player.zeroIndexedSpace]
+      playerCounter++
+    }
+
+    players.filterNot { it.points >= 1000 }.sumOf { it.points } * die.rollCount
+  }
+
+  fun partTwo(fileName: String) = generatorFactory.forFile(fileName).readAs(::player) { input ->
+    val d = input.toList()
+    val p1 = d.first().let { it.zeroIndexedSpace to it.points }
+    val p2 = d.last().let { it.zeroIndexedSpace to it.points }
+
+    val firstGame = Game(p1, p2, 1, 0)
+
+    val moveSets = mapOf(
+      3 to 1,
+      4 to 3,
+      5 to 6,
+      6 to 7,
+      7 to 6,
+      8 to 3,
+      9 to 1
+    )
+
+    var games = mutableListOf(firstGame)
+
+    val winCounts = mutableMapOf<Int, Long>().apply {
+      this[0] = 0
+      this[1] = 0
+    }
+
+    while(games.isNotEmpty()) {
+      val newGames = mutableListOf<Game>()
+
+      games.forEachIndexed { i, game ->
+        moveSets.forEach { (dieSum, newUniverseCount) ->
+          val (playerAIndex, playerAPoints) = game.player1
+          val (playerBIndex, playerBPoints) = game.player2
+          val newIndex = (playerAIndex + dieSum) % 10
+          val newPoints = playerAPoints + BOARD[newIndex]
+
+          if (newPoints >= 21) {
+            winCounts[game.nextPlayer] = winCounts[game.nextPlayer]!! + (game.universesIExistIn * newUniverseCount)
+          } else {
+            newGames.add(
+              Game(
+                player1 = playerBIndex to playerBPoints,
+                player2 = newIndex to newPoints,
+                universesIExistIn = game.universesIExistIn * newUniverseCount,
+                nextPlayer = (game.nextPlayer + 1) % 2
+              )
+            )
+          }
+        }
+      }
+
+      games = newGames
+    }
+
+    winCounts.maxOf { it.value }
+  }
+
+  data class Game(val player1: Pair<Int, Int>, val player2: Pair<Int, Int>, val universesIExistIn: Long, val nextPlayer: Int)
+
+  data class Player(val id: Int, var zeroIndexedSpace: Int, var points: Int = 0)
+
+  class DeterministicD100 {
+    private var result = 100
+    var rollCount: Long = 0
+
+    fun roll(): Int {
+      rollCount++
+      result++
+      if (result > 100) {
+        result = 1
+      }
+      return result
+    }
+  }
+
+  private fun player(line: String) = Player(
+    Character.getNumericValue(line[7]),
+    Character.getNumericValue(line[line.length - 1]) - 1
+  )
+}
--- src/test/kotlin/me/peckb/aoc/_2021/TestDayComponent.kt
+++ src/test/kotlin/me/peckb/aoc/_2021/TestDayComponent.kt
@@ -21,6 +21,7 @@ import me.peckb.aoc._2021.calendar.day17.Day17Test
 import me.peckb.aoc._2021.calendar.day18.Day18Test
 import me.peckb.aoc._2021.calendar.day19.Day19Test
 import me.peckb.aoc._2021.calendar.day20.Day20Test
+import me.peckb.aoc._2021.calendar.day21.Day21Test
 import javax.inject.Singleton
 
 @Singleton
@@ -46,4 +47,5 @@ internal interface TestDayComponent : DayComponent {
   fun inject(day18Test: Day18Test)
   fun inject(day19Test: Day19Test)
   fun inject(day20Test: Day20Test)
+  fun inject(day21Test: Day21Test)
 }
