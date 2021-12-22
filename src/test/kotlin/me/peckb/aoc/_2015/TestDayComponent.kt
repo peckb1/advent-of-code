2765216d879be6bee96789901ebcd140c111e594
Brian Peck <peckb@squareup.com>
2021-12-21 20:22:47 -0800

    Day 05 2015 (#26)

--- src/test/kotlin/me/peckb/aoc/_2015/TestDayComponent.kt
+++ src/test/kotlin/me/peckb/aoc/_2015/TestDayComponent.kt
@@ -7,6 +7,7 @@ import me.peckb.aoc._2015.calendar.day01.Day01Test
 import me.peckb.aoc._2015.calendar.day02.Day02Test
 import me.peckb.aoc._2015.calendar.day03.Day03Test
 import me.peckb.aoc._2015.calendar.day04.Day04Test
+import me.peckb.aoc._2015.calendar.day05.Day05Test
 import javax.inject.Singleton
 
 @Singleton
@@ -16,4 +17,5 @@ internal interface TestDayComponent : DayComponent {
   fun inject(day02Test: Day02Test)
   fun inject(day03Test: Day03Test)
   fun inject(day04Test: Day04Test)
+  fun inject(day05Test: Day05Test)
 }
