6849addc905295a76984b03428513284e0610de3
Brian Peck <peckb@squareup.com>
2021-12-24 12:15:10 -0800

    Day 24 2021 (#29)
    * Day 24 2021

---
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day24/Day24.kt
@@ -0,0 +1,76 @@
+package me.peckb.aoc._2021.calendar.day24
+
+import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
+import javax.inject.Inject
+import kotlin.math.max
+import kotlin.math.min
+
+typealias AddInstruction = Pair<Int, Int>
+
+class Day24 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
+  companion object {
+    const val MINIMUM_ALLOWED = 1
+    const val MAXIMUM_ALLOWED = 9
+  }
+
+  fun partOne(fileName: String) = generatorFactory.forFile(fileName).read { input ->
+    run(MAXIMUM_ALLOWED, input.chunked(18))
+  }
+
+  fun partTwo(fileName: String) = generatorFactory.forFile(fileName).read { input ->
+    run(MINIMUM_ALLOWED, input.chunked(18))
+  }
+
+  private fun run(idealResult: Int, blocks: Sequence<List<String>>): Long {
+    val result = MutableList(14) { 0 }
+    val addYStack = ArrayDeque<AddInstruction>()
+
+    blocks.forEachIndexed { mainResultIndex, instructions ->
+      // note: the input has an equal number of `div z 26` and `div z 1`
+      // the `div z 26` are the inputs that actually determine the values in the result
+      // the `div z 1` values are purely derivative from the previous (unused) `add y [value]` instruction
+      //   we keep track of "unused" by using a stack of addY(s)
+      if (instructions[4] == "div z 26") {
+        @Suppress("UnnecessaryVariable")
+        val addXIndex = mainResultIndex
+        val addXValue = instructions[5].substringAfterLast(" ").toInt()
+        val (addYIndex, addYValue) = addYStack.removeLast()
+        // janky bit of "reading the input here"
+        // whenever `div z 26` shows up, the `value` inside `add x [value]` is always negative
+        // so we get the difference between the Y we want to add, and the X we want to subtract
+        // REMINDER: the `add y [value]` is actually the value from the previous unused `div z 1` instruction
+        val diff = addYValue + addXValue
+
+        if (diff >= 0) {
+          // if the difference is a positive number:
+          //   the `min value` for our x index            would be `1 + diff`
+          //   the `min value` for our derivative y index would be `1`
+          //   the `max value` for our x index            would be `9`
+          //   the `max value` for our derivative y index would be `9 - diff`
+          // we can simplify those commands by
+          //   always adding      for `addXIndex` (stopping at the max allowed)
+          //   always subtracting for `addYIndex` (stopping at the min allowed)
+          result[addXIndex] = min(idealResult + diff, MAXIMUM_ALLOWED)
+          result[addYIndex] = max(idealResult - diff, MINIMUM_ALLOWED)
+        } else {
+          // if the difference is a negative number, we need to invert the positive difference logic:
+          //   the `min value` for our x index            would be `1`
+          //   the `min value` for our derivative y index would be `1 - diff` (diff is < 0, so we're adding)
+          //   the `max value` for our x index            would be `9 + diff` (diff is < 0, so we're subtracting)
+          //   the `max value` for our derivative y index would be `9`
+          // we can simplify those commands by
+          //   always "adding"      for `addXIndex` (stopping at the min allowed, since we're a negative)
+          //   always "subtracting" for `addYIndex` (stopping at the max allowed, since we're a negative)
+          result[addXIndex] = max(idealResult + diff, MINIMUM_ALLOWED)
+          result[addYIndex] = min(idealResult - diff, MAXIMUM_ALLOWED)
+        }
+      } else {
+        // build up the stack of all of the "add y [value]" in a pair of the
+        // index that we found the instruction, and the [value] to add it to
+        addYStack.add(mainResultIndex to instructions[15].substringAfterLast(" ").toInt())
+      }
+    }
+
+    return result.joinToString("").toLong()
+  }
+}
--- src/test/kotlin/me/peckb/aoc/_2021/TestDayComponent.kt
+++ src/test/kotlin/me/peckb/aoc/_2021/TestDayComponent.kt
@@ -25,6 +25,7 @@ import me.peckb.aoc._2021.calendar.day19.Day19Test
 import me.peckb.aoc._2021.calendar.day20.Day20Test
 import me.peckb.aoc._2021.calendar.day21.Day21Test
 import me.peckb.aoc._2021.calendar.day22.Day22Test
+import me.peckb.aoc._2021.calendar.day24.Day24Test
 import javax.inject.Singleton
 
 @Singleton
@@ -52,4 +53,5 @@ internal interface TestDayComponent : DayComponent {
   fun inject(day20Test: Day20Test)
   fun inject(day21Test: Day21Test)
   fun inject(day22Test: Day22Test)
+  fun inject(day24Test: Day24Test)
 }
