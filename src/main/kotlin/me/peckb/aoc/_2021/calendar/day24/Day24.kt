f90dee65e545d764baabdd0bed31f26bcff126fc
Brian Peck <peckb@squareup.com>
2021-12-24 11:47:18 -0800

    a bit more cleanup/documentation

--- src/main/kotlin/me/peckb/aoc/_2021/calendar/day24/Day24.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day24/Day24.kt
@@ -2,30 +2,28 @@ package me.peckb.aoc._2021.calendar.day24
 
 import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
 import javax.inject.Inject
+import kotlin.math.max
+import kotlin.math.min
 
 typealias AddInstruction = Pair<Int, Int>
 
 class Day24 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
   companion object {
-    const val MIN = true
-    const val MAX = false
-
-    const val IDEAL_MIN = 1
-    const val IDEAL_MAX = 9
+    const val MINIMUM_ALLOWED = 1
+    const val MAXIMUM_ALLOWED = 9
   }
 
   fun partOne(fileName: String) = generatorFactory.forFile(fileName).read { input ->
-    run(MAX, input.chunked(18))
+    run(MAXIMUM_ALLOWED, input.chunked(18))
   }
 
   fun partTwo(fileName: String) = generatorFactory.forFile(fileName).read { input ->
-    run(MIN, input.chunked(18))
+    run(MINIMUM_ALLOWED, input.chunked(18))
   }
 
-  private fun run(min: Boolean, blocks: Sequence<List<String>>): Long {
+  private fun run(idealResult: Int, blocks: Sequence<List<String>>): Long {
     val result = MutableList(14) { 0 }
     val addYStack = ArrayDeque<AddInstruction>()
-    val idealResult = if (min) IDEAL_MIN else IDEAL_MAX
 
     blocks.forEachIndexed { mainResultIndex, instructions ->
       // note: the input has an equal number of `div z 26` and `div z 1`
@@ -33,10 +31,12 @@ class Day24 @Inject constructor(private val generatorFactory: InputGeneratorFact
       // the `div z 1` values are purely derivative from the most previous, but not used
       // `add y [value]` instruction
       if (instructions[4] == "div z 26") {
+        @Suppress("UnnecessaryVariable")
+        val addXIndex = mainResultIndex
         val addXValue = instructions[5].substringAfterLast(" ").toInt()
         val (addYIndex, addYValue) = addYStack.removeFirst()
         // janky bit of "reading the input here"
-        // whenever `div z 26` shows up the `value` inside `add x [value]` is always negative
+        // whenever `div z 26` shows up, the `value` inside `add x [value]` is always negative
         // so we get the difference between the Y we want to add, and the X we want to subtract
         // NOTE: the y `value` is actually the value from the previous instruction
         val diff = addYValue + addXValue
@@ -50,16 +50,22 @@ class Day24 @Inject constructor(private val generatorFactory: InputGeneratorFact
           //   the `min value` for our derivative y index would be `1`
           //   the `max value` for our x index            would be `9`
           //   the `max value` for our derivative y index would be `9 - diff`
-          result[mainResultIndex] = if (min) idealResult + diff else idealResult
-          result[addYIndex]       = if (min) idealResult        else idealResult - diff
+          // we can simplify those commands by
+          //   always adding      for `addXIndex` (stopping at the max allowed)
+          //   always subtracting for `addYIndex` (stopping at the min allowed)
+          result[addXIndex] = min(idealResult + diff, MAXIMUM_ALLOWED)
+          result[addYIndex] = max(idealResult - diff, MINIMUM_ALLOWED)
         } else {
           // if the difference is a negative number, we need to invert the positive difference logic:
           //   the `min value` for our x index            would be `1`
           //   the `min value` for our derivative y index would be `1 - diff` (diff is < 0, so we're adding)
           //   the `max value` for our x index            would be `9 + diff` (diff is < 0, so we're subtracting)
           //   the `max value` for our derivative y index would be `9`
-          result[mainResultIndex] = if (min) idealResult        else idealResult + diff
-          result[addYIndex]       = if (min) idealResult - diff else idealResult
+          // we can simplify those commands by
+          //   always "adding"      for `addXIndex` (stopping at the min allowed, since we're a negative)
+          //   always "subtracting" for `addYIndex` (stopping at the max allowed, since we're a negative)
+          result[addXIndex] = max(idealResult + diff, MINIMUM_ALLOWED)
+          result[addYIndex] = min(idealResult - diff, MAXIMUM_ALLOWED)
         }
       } else {
         // build up a stack of all of the "add y [value]" in a pair of the
