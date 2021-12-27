f856f9c23a52b173be88d9c12ce4209c8e8c9e95
Brian Peck <peckb@squareup.com>
2021-12-27 08:52:26 -0800

    faster, but still to painfully slow

--- src/main/kotlin/me/peckb/aoc/_2021/calendar/day23/Day23.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day23/Day23.kt
@@ -4,19 +4,27 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
 import me.peckb.aoc._2021.calendar.day23.Resident.AMBER
 import me.peckb.aoc._2021.calendar.day23.Resident.BRONZE
 import me.peckb.aoc._2021.calendar.day23.Resident.COPPER
 import me.peckb.aoc._2021.calendar.day23.Resident.DESERT
 import me.peckb.aoc.generators.InputGenerator
+import java.util.concurrent.atomic.AtomicInteger
 import javax.inject.Inject
+import kotlin.Int.Companion.MAX_VALUE
 
 class Day23 @Inject constructor(private val generatorFactory: InputGenerator.InputGeneratorFactory) {
   fun partOne(fileName: String) = generatorFactory.forFile(fileName).read { input ->
     val floor = readInput(input)
     val moves = floor.availableMoves()
 
+    val cheapestCost = AtomicInteger(MAX_VALUE)
+
     runBlocking {
+      val started = moves.size
+      val completed = AtomicInteger(0)
       val cheapestMoves = moves.map { movement: Movement ->
         async(context = Dispatchers.Default) {
           // copy floor
@@ -25,16 +33,15 @@ class Day23 @Inject constructor(private val generatorFactory: InputGenerator.Inp
           // make move manually
           myFloor.makeMove(movement)
 
-          println("Starting search after $movement")
+          // println("Starting search after $movement")
 
           // start checking every move synchronously for each of these
-          myFloor.findCheapestRearrangement(ArrayDeque(listOf(movement))).also {
-            println("Finished $movement")
+          myFloor.findCheapestRearrangement(ArrayDeque(listOf(movement)), movement.cost, cheapestCost).also {
+            // println("Finished $movement")
+            println("${completed.incrementAndGet()} / $started done.")
           }
-        }//.await()
+        }
       }.awaitAll().mapNotNull { it }
-
-      // println(cheapestMoves)
 
       if (cheapestMoves.isEmpty()) {
         null
--- src/main/kotlin/me/peckb/aoc/_2021/calendar/day23/Floor.kt
+++ src/main/kotlin/me/peckb/aoc/_2021/calendar/day23/Floor.kt
@@ -1,13 +1,12 @@
 package me.peckb.aoc._2021.calendar.day23
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.runBlocking
 import me.peckb.aoc._2021.calendar.day23.Movement.EnterHallFromRoom
 import me.peckb.aoc._2021.calendar.day23.Movement.EnterRoomFromHall
 import me.peckb.aoc._2021.calendar.day23.Movement.EnterRoomFromRoom
 import java.util.PriorityQueue
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.Int.Companion.MAX_VALUE
+import kotlin.math.min
 
 class Floor(val hallway: Array<Resident?>, val rooms: Map<Resident, Array<Resident?>>) {
   fun cloneFloor() = Floor(hallway.copyOf(), rooms.mapValues { it.value.copyOf() })
@@ -15,19 +14,9 @@ class Floor(val hallway: Array<Resident?>, val rooms: Map<Resident, Array<Reside
   fun availableMoves(): PriorityQueue<Movement> {
     val queue = PriorityQueue<Movement>(compareBy { it.cost })
 
-    generateEnterHallFromRoomMoves().forEach { queue.add(it) }
-    generateEnterRoomFromRoomMoves().forEach { queue.add(it) }
     generateEnterRoomFromHallMoves().forEach { queue.add(it) }
-
-    // return PriorityQueue<Movement>(compareBy { it.cost }).apply {
-    //   runBlocking {
-    //     listOf(
-    //       async { generateEnterHallFromRoomMoves().forEach { add(it) } },
-    //       async { generateEnterRoomFromRoomMoves().forEach { add(it) } },
-    //       async { generateEnterRoomFromHallMoves().forEach { add(it) } },
-    //     ).awaitAll()
-    //   }
-    // }
+    val roomToRoomMoves = generateEnterRoomFromRoomMoves().onEach { queue.add(it) }
+    generateEnterHallFromRoomMoves(roomToRoomMoves).forEach { queue.add(it) }
 
     return queue
   }
@@ -66,17 +55,14 @@ class Floor(val hallway: Array<Resident?>, val rooms: Map<Resident, Array<Reside
     }
   }
 
-  fun findCheapestRearrangement(currentMoves: ArrayDeque<Movement>) : Pair<Int, List<Movement>>? {
-    // println(this)
-    // println()
+  fun findCheapestRearrangement(currentMoves: ArrayDeque<Movement>, costSoFar: Int, cheapestCost: AtomicInteger) : Pair<Int, List<Movement>>? {
     val moves = availableMoves()
 
     var cheapestRouteFound: Pair<Int, List<Movement>>? = null
-    val costSoFar = currentMoves.sumOf { it.cost }
 
     moveCheck@ while (moves.isNotEmpty()) {
       val nextMove = moves.remove()
-      if (costSoFar + nextMove.cost > cheapestRouteFound?.first ?: MAX_VALUE) {
+      if (costSoFar + nextMove.cost > cheapestCost.get()) {
         continue@moveCheck
       }
 
@@ -85,11 +71,12 @@ class Floor(val hallway: Array<Resident?>, val rooms: Map<Resident, Array<Reside
 
       if (isComplete()) {
         return ((costSoFar + nextMove.cost) to currentMoves.toList()).also {
+          cheapestCost.getAndUpdate { current -> min(current, it.first) }
           currentMoves.removeLast()
           undoMove(nextMove)
         }
       } else {
-        val possibleRoute = findCheapestRearrangement(currentMoves)
+        val possibleRoute = findCheapestRearrangement(currentMoves, costSoFar + nextMove.cost, cheapestCost)
         if (cheapestRouteFound == null) {
           cheapestRouteFound = possibleRoute
         }
@@ -102,85 +89,62 @@ class Floor(val hallway: Array<Resident?>, val rooms: Map<Resident, Array<Reside
       undoMove(nextMove)
     }
 
-    // println("<--")
     return cheapestRouteFound
   }
 
-  private fun generateEnterHallFromRoomMoves(): List<Movement> {
-    fun findValidEnterHallwayMovements(roomOwner: Resident, hallIndex: Int, roomIndex: Int, resident: Resident): List<Movement> {
-      // confirm that I want to leave
-      val nonOwnersBehindMe = (roomIndex+1 until rooms[roomOwner]!!.size).any { rooms[roomOwner]!![it] != roomOwner }
-      val shouldLeave = resident != roomOwner || nonOwnersBehindMe
-      if (!shouldLeave) return emptyList()
+  private fun generateEnterHallFromRoomMoves(roomToRoomMoves: List<EnterRoomFromRoom>): List<EnterHallFromRoom> {
+    val peopleToIgnore = roomToRoomMoves.map { it.sourceRoomOwner to it.sourceRoomIndex }
 
-      // confirm that the room from me to doorway is empty
-      val neighborsInMyWay = (0 until roomIndex).mapNotNull { rooms[roomOwner]!![it] }
-      val pathToDoorwayClear = neighborsInMyWay.isEmpty() && hallway[hallIndex] == null
-      if (!pathToDoorwayClear) return emptyList()
-
-      val costToEnterDoorway = resident.movementCost * (roomIndex + 1)
-      val movements = mutableListOf<Movement>()
-
-      // see if we can walk to the left outside of the doorway
-      val leftSteps = hallIndex - 1 downTo 0
-      val rightSteps = hallIndex + 1 until hallway.size
-
-      fun tryFindEnterHallFromRoomMovements(possibleValues: IntProgression) {
-        var steps = 0
-        hallwaySearch@ for (possibleHallIndex in possibleValues) {
-          steps++
-          if (hallway[possibleHallIndex] == null) {
-            val cost = (steps * resident.movementCost) + costToEnterDoorway
-            movements.add(EnterHallFromRoom(roomOwner, roomIndex, possibleHallIndex, cost))
-          } else {
-            break@hallwaySearch
-          }
-        }
-      }
-
-      tryFindEnterHallFromRoomMovements(leftSteps)
-      tryFindEnterHallFromRoomMovements(rightSteps)
-
-      return movements
-    }
-
-    return mutableListOf<Movement>().apply {
+    return mutableListOf<EnterHallFromRoom>().apply {
       rooms.forEach { room ->
         room.value.forEachIndexed { roomIndex, resident ->
-          resident?.let { addAll(findValidEnterHallwayMovements(room.key, room.key.doorwayIndex, roomIndex, it)) }
+          if (!peopleToIgnore.contains(resident to roomIndex)) {
+            resident?.let { addAll(findValidEnterHallwayMovements(room.key, room.key.doorwayIndex, roomIndex, it)) }
+          }
         }
       }
     }
   }
 
-  private fun generateEnterRoomFromRoomMoves(): List<Movement> {
-    fun findValidRoomTransferMovements(roomOwner: Resident, hallIndex: Int, roomIndex: Int, resident: Resident): Movement? {
-      // confirm that I want to leave
-      val imInMyOwnRoom = roomOwner == resident
-      val nonOwnersBehindMe = (roomIndex+1 until rooms[roomOwner]!!.size).mapNotNull { rooms[roomOwner]!![it] }
-      val shouldLeave = !imInMyOwnRoom && (resident != roomOwner || nonOwnersBehindMe.isNotEmpty())
-      if (!shouldLeave) return null
+  private fun findValidEnterHallwayMovements(roomOwner: Resident, hallIndex: Int, roomIndex: Int, resident: Resident): List<EnterHallFromRoom> {
+    // confirm that I want to leave
+    val nonOwnersBehindMe = (roomIndex+1 until rooms[roomOwner]!!.size).any { rooms[roomOwner]!![it] != roomOwner }
+    val shouldLeave = resident != roomOwner || nonOwnersBehindMe
+    if (!shouldLeave) return emptyList()
 
-      // confirm that the room from me to doorway is empty
-      val neighborsInMyWay = (0 until roomIndex).mapNotNull { rooms[roomOwner]!![it] }
-      val pathToDoorwayClear = neighborsInMyWay.isEmpty() && hallway[hallIndex] == null
-      if (!pathToDoorwayClear) return null
+    // confirm that the room from me to doorway is empty
+    val neighborsInMyWay = (0 until roomIndex).mapNotNull { rooms[roomOwner]!![it] }
+    val pathToDoorwayClear = neighborsInMyWay.isEmpty() && hallway[hallIndex] == null
+    if (!pathToDoorwayClear) return emptyList()
 
-      // confirm that the path from my room to my target room is clear
-      val pathToTargetRoom = roomOwner.pathToRoom(resident)
-      val someoneInTheWay = pathToTargetRoom.any { hallway[it] != null }
-      if (someoneInTheWay) return null
+    val costToEnterDoorway = resident.movementCost * (roomIndex + 1)
+    val movements = mutableListOf<EnterHallFromRoom>()
 
-      // confirm that the only existing people in my target room want to stay there
-      val nonResidentsPresent = rooms[resident]!!.any { it != null && it != resident }
-      if (nonResidentsPresent) return null
+    // see if we can walk to the left outside of the doorway
+    val leftSteps = hallIndex - 1 downTo 0
+    val rightSteps = hallIndex + 1 until hallway.size
 
-      val targetRoomIndex = ((rooms[resident]!!.size - 1) downTo 0).first { rooms[resident]!![it] == null }
-      val cost = (pathToTargetRoom.count() + targetRoomIndex + roomIndex + 1) * resident.movementCost
-      return EnterRoomFromRoom(roomOwner, roomIndex, resident, targetRoomIndex, cost)
+    movements.tryFindEnterHallFromRoomMovements(resident, costToEnterDoorway, roomOwner, roomIndex, leftSteps)
+    movements.tryFindEnterHallFromRoomMovements(resident, costToEnterDoorway, roomOwner, roomIndex, rightSteps)
+
+    return movements
+  }
+
+  private fun MutableList<EnterHallFromRoom>.tryFindEnterHallFromRoomMovements(resident: Resident, costToEnterDoorway: Int, roomOwner: Resident, roomIndex: Int, possibleValues: IntProgression) {
+    var steps = 0
+    hallwaySearch@ for (possibleHallIndex in possibleValues) {
+      steps++
+      if (hallway[possibleHallIndex] == null) {
+        val cost = (steps * resident.movementCost) + costToEnterDoorway
+        add(EnterHallFromRoom(roomOwner, roomIndex, possibleHallIndex, cost))
+      } else {
+        break@hallwaySearch
+      }
     }
+  }
 
-    return mutableListOf<Movement>().apply {
+  private fun generateEnterRoomFromRoomMoves(): List<EnterRoomFromRoom> {
+    return mutableListOf<EnterRoomFromRoom>().apply {
       rooms.forEach { room ->
         room.value.forEachIndexed { roomIndex, resident ->
           resident?.also { findValidRoomTransferMovements(room.key, room.key.doorwayIndex, roomIndex, it)?.also(::add) }
@@ -189,28 +153,54 @@ class Floor(val hallway: Array<Resident?>, val rooms: Map<Resident, Array<Reside
     }
   }
 
-  private fun generateEnterRoomFromHallMoves(): List<Movement> {
-    fun findValidEnterRoomMovements(hallIndex: Int, resident: Resident): Movement? {
-      // confirm that the path to my target room is clear
-      val step = if (hallIndex < resident.doorwayIndex) 1 else -1
-      val pathToTargetRoom = IntProgression.fromClosedRange(hallIndex + step, resident.doorwayIndex, step)
-      val neighborsInMyWay = pathToTargetRoom.any { hallway[it] != null }
-      if (neighborsInMyWay) return null
+  private fun findValidRoomTransferMovements(roomOwner: Resident, hallIndex: Int, roomIndex: Int, resident: Resident): EnterRoomFromRoom? {
+    // confirm that I want to leave
+    val imInMyOwnRoom = roomOwner == resident
+    val nonOwnersBehindMe = (roomIndex+1 until rooms[roomOwner]!!.size).mapNotNull { rooms[roomOwner]!![it] }
+    val shouldLeave = !imInMyOwnRoom && (resident != roomOwner || nonOwnersBehindMe.isNotEmpty())
+    if (!shouldLeave) return null
 
-      // confirm that the only existing people in my target room want to stay there
-      val nonResidentsPresent = rooms[resident]!!.any { it != null && it != resident }
-      if (nonResidentsPresent) return null
+    // confirm that the room from me to doorway is empty
+    val neighborsInMyWay = (0 until roomIndex).mapNotNull { rooms[roomOwner]!![it] }
+    val pathToDoorwayClear = neighborsInMyWay.isEmpty() && hallway[hallIndex] == null
+    if (!pathToDoorwayClear) return null
 
-      val targetRoomIndex = ((rooms[resident]!!.size - 1) downTo 0).first { rooms[resident]!![it] == null }
-      val cost = (pathToTargetRoom.count() + targetRoomIndex + 1) * resident.movementCost
-      return EnterRoomFromHall(hallIndex, resident, targetRoomIndex, cost)
-    }
+    // confirm that the path from my room to my target room is clear
+    val pathToTargetRoom = roomOwner.pathToRoom(resident)
+    val someoneInTheWay = pathToTargetRoom.any { hallway[it] != null }
+    if (someoneInTheWay) return null
 
-    return mutableListOf<Movement>().apply {
+    // confirm that the only existing people in my target room want to stay there
+    val nonResidentsPresent = rooms[resident]!!.any { it != null && it != resident }
+    if (nonResidentsPresent) return null
+
+    val targetRoomIndex = ((rooms[resident]!!.size - 1) downTo 0).first { rooms[resident]!![it] == null }
+    val cost = (pathToTargetRoom.count() + targetRoomIndex + roomIndex + 1) * resident.movementCost
+    return EnterRoomFromRoom(roomOwner, roomIndex, resident, targetRoomIndex, cost)
+  }
+
+  private fun generateEnterRoomFromHallMoves(): List<EnterRoomFromHall> {
+    return mutableListOf<EnterRoomFromHall>().apply {
       hallway.forEachIndexed { hallwayIndex, hallwaySpace ->
         hallwaySpace?.also { findValidEnterRoomMovements(hallwayIndex, it)?.also(::add) }
       }
     }
+  }
+
+  private fun findValidEnterRoomMovements(hallIndex: Int, resident: Resident): EnterRoomFromHall? {
+    // confirm that the path to my target room is clear
+    val step = if (hallIndex < resident.doorwayIndex) 1 else -1
+    val pathToTargetRoom = IntProgression.fromClosedRange(hallIndex + step, resident.doorwayIndex, step)
+    val neighborsInMyWay = pathToTargetRoom.any { hallway[it] != null }
+    if (neighborsInMyWay) return null
+
+    // confirm that the only existing people in my target room want to stay there
+    val nonResidentsPresent = rooms[resident]!!.any { it != null && it != resident }
+    if (nonResidentsPresent) return null
+
+    val targetRoomIndex = ((rooms[resident]!!.size - 1) downTo 0).first { rooms[resident]!![it] == null }
+    val cost = (pathToTargetRoom.count() + targetRoomIndex + 1) * resident.movementCost
+    return EnterRoomFromHall(hallIndex, resident, targetRoomIndex, cost)
   }
 
   private fun isComplete() = rooms.all { room ->
