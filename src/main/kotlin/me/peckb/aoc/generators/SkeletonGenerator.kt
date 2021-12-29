d9e86bdff3260c79e3bf2e5d310e32f1d90845cd
Brian Peck <peckb@squareup.com>
2021-12-28 19:14:53 -0800

    test file created

--- src/main/kotlin/me/peckb/aoc/generators/SkeletonGenerator.kt
+++ src/main/kotlin/me/peckb/aoc/generators/SkeletonGenerator.kt
@@ -3,6 +3,9 @@ package me.peckb.aoc.generators
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier.CONST
+import com.squareup.kotlinpoet.KModifier.INTERNAL
+import com.squareup.kotlinpoet.KModifier.LATEINIT
 import com.squareup.kotlinpoet.KModifier.PRIVATE
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
@@ -26,6 +29,7 @@ class SkeletonGenerator(private val year: String, private val day: String) {
     generateREADME()
     generateTestClass()
     generateInput()
+    alterTestDayComponent()
   }
 
   private fun generateDayClass() {
@@ -71,8 +75,8 @@ class SkeletonGenerator(private val year: String, private val day: String) {
 
     file.writeTo(File(SRC_DIRECTORY))
 
-    // and the safety import of `kotlin.String`
     // remove all the extraneous `public` modifiers
+    // and the safety import of `kotlin.String`
     val path = Paths.get("$SRC_DIRECTORY/me/peckb/aoc/_$year/calendar/day$day/Day$day.kt")
     val content = String(Files.readAllBytes(path))
       .replace("public ".toRegex(), "")
@@ -96,11 +100,68 @@ class SkeletonGenerator(private val year: String, private val day: String) {
   }
 
   private fun generateTestClass() {
+    val file = FileSpec.builder("me.peckb.aoc._$year.calendar.day$day", "Day${day}Test")
+      .addImport(ClassName("me.peckb.aoc", "_$year"), "DaggerTestDayComponent")
+      .addImport(ClassName("org.junit.jupiter.api", "Assertions"), "assertEquals")
+      .addType(
+        TypeSpec.classBuilder("Day${day}Test")
+          .addModifiers(INTERNAL)
+          .addType(
+            TypeSpec.companionObjectBuilder()
+              .addProperty(
+                PropertySpec.builder("DAY_$day", String::class)
+                  .initializer("%S", "$TEST_DIRECTORY/resources/$year/day$day.input")
+                  .addModifiers(PRIVATE, CONST)
+                  .build()
+              )
+              .build()
+          )
+          .addFunction(
+            FunSpec.builder("setup")
+              .addAnnotation(ClassName("org.junit.jupiter.api", "BeforeEach"))
+              .addStatement("DaggerTestDayComponent.create().inject(this)")
+              .build()
+          )
+          .addProperty(
+            PropertySpec.builder("day$day", ClassName("me.peckb.aoc._$year.calendar.day$day", "Day$day"))
+              .addAnnotation(Inject::class)
+              .addModifiers(LATEINIT)
+              .mutable(true)
+              .build()
+          ).addFunction(
+            FunSpec.builder("testDay${day}PartOne")
+              .addAnnotation(ClassName("org.junit.jupiter.api", "Test"))
+              .addStatement("assertEquals(-1, day${day}.partOne(DAY_${day}))")
+              .build()
+          ).addFunction(
+            FunSpec.builder("testDay${day}PartTwo")
+              .addAnnotation(ClassName("org.junit.jupiter.api", "Test"))
+              .addStatement("assertEquals(-1, day${day}.partTwo(DAY_${day}))")
+              .build()
+          )
+          .build()
+      )
+      .build()
 
+    file.writeTo(File("$TEST_DIRECTORY/kotlin"))
+
+    // remove all the extraneous `public` and `:Unit` modifiers
+    // and the safety imports of `kotlin.String` and `kotlin.Unit`
+    val path = Paths.get("$TEST_DIRECTORY/kotlin/me/peckb/aoc/_$year/calendar/day$day/Day${day}Test.kt")
+    val content = String(Files.readAllBytes(path))
+      .replace("public ".toRegex(), "")
+      .replace(": Unit".toRegex(), "")
+      .replace("import kotlin.String".toRegex(), "")
+      .replace("import kotlin.Unit".toRegex(), "")
+    Files.write(path, content.toByteArray())
   }
 
   private fun generateInput() {
     val path = Paths.get("$TEST_DIRECTORY/resources/$year/day$day.input")
-    Files.createFile(path)
+    Files.write(path, "".toByteArray())
+  }
+
+  private fun alterTestDayComponent() {
+
   }
 }
