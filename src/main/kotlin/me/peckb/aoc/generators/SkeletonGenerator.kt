34cdbafdf307c00adcf7dd5970c711a2f13342e8
Brian Peck <peckb@squareup.com>
2021-12-28 19:18:28 -0800

    alter test component

--- src/main/kotlin/me/peckb/aoc/generators/SkeletonGenerator.kt
+++ src/main/kotlin/me/peckb/aoc/generators/SkeletonGenerator.kt
@@ -162,6 +162,10 @@ class SkeletonGenerator(private val year: String, private val day: String) {
   }
 
   private fun alterTestDayComponent() {
-
+    val path = Paths.get("$TEST_DIRECTORY/kotlin/me/peckb/aoc/_$year/TestDayComponent.kt")
+    val content = String(Files.readAllBytes(path))
+      .replace("}".toRegex(), "  fun inject(day${day}Test: Day${day}Test)\n}")
+      .replace("import javax.inject.Singleton".toRegex(), "import me.peckb.aoc._$year.calendar.day$day.Day${day}Test\nimport javax.inject.Singleton")
+    Files.write(path, content.toByteArray())
   }
 }
