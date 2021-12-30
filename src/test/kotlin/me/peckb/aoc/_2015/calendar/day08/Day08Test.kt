de05ab30632e73d2603558c2cd72cab04f7ecafc
Brian Peck <peckb@squareup.com>
2021-12-29 19:07:56 -0800

    Day08 2021 (#34)

---
+++ src/test/kotlin/me/peckb/aoc/_2015/calendar/day08/Day08Test.kt
@@ -0,0 +1,33 @@
+package me.peckb.aoc._2015.calendar.day08
+
+import javax.inject.Inject
+
+
+import me.peckb.aoc._2015.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Day08Test {
+  @Inject
+  lateinit var day08: Day08
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Test
+  fun testDay08PartOne() {
+    assertEquals(1350, day08.partOne(DAY_08))
+  }
+
+  @Test
+  fun testDay08PartTwo() {
+    assertEquals(2085, day08.partTwo(DAY_08))
+  }
+
+  companion object {
+    private const val DAY_08: String = "src/test/resources/2015/day08.input"
+  }
+}
