dd05f7f024fb3e4014e9b15beab68fd7296a824f
Brian Peck <peckb@squareup.com>
2022-01-12 18:22:57 -0800

    part 1

---
+++ src/main/kotlin/me/peckb/aoc/_2017/calendar/day17/Day17.kt
@@ -0,0 +1,25 @@
+package me.peckb.aoc._2017.calendar.day17
+
+import javax.inject.Inject
+
+import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
+
+class Day17 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
+  fun partOne(filename: String) = generatorFactory.forFile(filename).readOne { input ->
+    val stepSize = input.toInt()
+    val data = mutableListOf(0)
+
+    var currentIndex = 0
+    (1 .. 2017).forEach { n ->
+      val indexToInsertAfter = (currentIndex + stepSize) % data.size
+      currentIndex = indexToInsertAfter + 1
+      data.add(currentIndex, n)
+    }
+
+    data[data.indexOf(2017) + 1]
+  }
+
+  fun partTwo(filename: String) = generatorFactory.forFile(filename).readOne { input ->
+    -1
+  }
+}
---
+++ src/main/kotlin/me/peckb/aoc/_2017/calendar/day17/README.md
@@ -0,0 +1,1 @@
+## [Day 17: Spinlock](https://adventofcode.com/2017/day/17)
