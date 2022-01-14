b6f554025f30e36bfeffaec773f3f47c2dd7d618
Brian Peck <peckb@squareup.com>
2022-01-13 22:22:26 -0800

    Day 21 2017 (#98)
    * Day 21 2017 Skeleton
    
    * Implementation

---
+++ src/main/kotlin/me/peckb/aoc/_2017/calendar/day21/README.md
@@ -0,0 +1,1 @@
+## [Day 21: Fractal Art](https://adventofcode.com/2017/day/21)
---
+++ src/test/kotlin/me/peckb/aoc/_2017/calendar/day21/Day21Test.kt
@@ -0,0 +1,33 @@
+package me.peckb.aoc._2017.calendar.day21
+
+import javax.inject.Inject
+
+
+import me.peckb.aoc._2017.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Day21Test {
+  @Inject
+  lateinit var day21: Day21
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Test
+  fun testDay21PartOne() {
+    assertEquals(155, day21.partOne(DAY_21))
+  }
+
+  @Test
+  fun testDay21PartTwo() {
+    assertEquals(2449665, day21.partTwo(DAY_21))
+  }
+
+  companion object {
+    private const val DAY_21: String = "src/test/resources/2017/day21.input"
+  }
+}
