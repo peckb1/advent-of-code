abcd882c31a010ecf51a459855e018928321abfd
Brian Peck <peckb@squareup.com>
2022-01-16 13:45:50 -0800

    cleanup

--- src/main/kotlin/me/peckb/aoc/_2018/calendar/day06/Day06.kt
+++ src/main/kotlin/me/peckb/aoc/_2018/calendar/day06/Day06.kt
@@ -10,30 +10,18 @@ import kotlin.math.abs
 class Day06 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
   fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::point) { input ->
     val points = input.toList()
+    val area = generateArea(points)
 
-    var top = MAX_VALUE
-    var bottom = MIN_VALUE
-    var left = MAX_VALUE
-    var right = MIN_VALUE
-
-    points.forEach { (x, y) ->
-      top = minOf(y, top)
-      bottom = maxOf(y, bottom)
-      left = minOf(x, left)
-      right = maxOf(x, right)
-    }
-
-    val pointsToIgnore = points.filter { (x, y) -> x == left || x == right || y == top || y == bottom }.toSet()
+    val pointsToIgnore = points.filter { (x, y) ->
+      x == area.left || x == area.right || y == area.top || y == area.bottom
+    }.toSet()
 
     val counts = mutableMapOf<Point, Int>()
-    (top .. bottom).forEach { y ->
-      (left..right).forEach { x ->
-        val distances: List<Pair<Point, Int>> = points.map { point ->
-          point to abs(x - point.x) + abs(y - point.y)
-        }.sortedBy { it.second }
-        val (closest, secondClosest) = distances.take(2)
-        if (closest.second < secondClosest.second) counts.merge(closest.first, 1, Int::plus)
-      }
+
+    forPoints(area) { x, y ->
+      val distances = points.map { it to abs(x - it.x) + abs(y - it.y) }.sortedBy { it.second }
+      val (closest, secondClosest) = distances.take(2)
+      if (closest.second < secondClosest.second) counts.merge(closest.first, 1, Int::plus)
     }
 
     counts.filterNot { pointsToIgnore.contains(it.key) }.maxOf { it.value }
@@ -41,7 +29,18 @@ class Day06 @Inject constructor(private val generatorFactory: InputGeneratorFact
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::point) { input ->
     val points = input.toList()
+    val area = generateArea(points)
 
+    var pointsWithinRegion = 0
+    forPoints(area) { x, y ->
+      val distanceSum = points.sumOf { point -> abs(x - point.x) + abs(y - point.y) }
+      if (distanceSum < DISTANCE) pointsWithinRegion++
+    }
+
+    pointsWithinRegion
+  }
+
+  private fun generateArea(points: List<Point>): Area {
     var top = MAX_VALUE
     var bottom = MIN_VALUE
     var left = MAX_VALUE
@@ -54,23 +53,24 @@ class Day06 @Inject constructor(private val generatorFactory: InputGeneratorFact
       right = maxOf(x, right)
     }
 
-    var pointsWithinRegion = 0
+    return Area(top, bottom, left, right)
+  }
 
-    (top .. bottom).forEach { y ->
-      (left..right).forEach { x ->
-        val distanceSum = points.sumOf { point -> abs(x - point.x) + abs(y - point.y) }
-        if (distanceSum < DISTANCE) pointsWithinRegion++
+  private fun forPoints(area: Area, handler: (x: Int, y: Int) -> Unit) {
+    (area.top..area.bottom).forEach { y ->
+      (area.left..area.right).forEach { x ->
+        handler(x, y)
       }
     }
-
-    pointsWithinRegion
   }
 
   private fun point(line: String) = line.split(", ").let { (x, y) -> Point(x.toInt(), y.toInt()) }
 
-  data class Point(val x: Int, val y: Int)
+  private data class Point(val x: Int, val y: Int)
+
+  private data class Area(val top: Int, val bottom: Int, val left: Int, val right: Int)
 
   companion object {
-    const val DISTANCE = 10_000
+    private const val DISTANCE = 10_000
   }
 }
