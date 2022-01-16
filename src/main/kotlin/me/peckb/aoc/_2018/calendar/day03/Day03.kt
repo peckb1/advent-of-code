2a7a19d5ce41218b5e9ce763621a27bf3f6c4aa6
Brian Peck <peckb@squareup.com>
2022-01-15 16:04:03 -0800

    remove zombie code

--- src/main/kotlin/me/peckb/aoc/_2018/calendar/day03/Day03.kt
+++ src/main/kotlin/me/peckb/aoc/_2018/calendar/day03/Day03.kt
@@ -53,27 +53,7 @@ class Day03 @Inject constructor(private val generatorFactory: InputGeneratorFact
     }
   }
 
-  data class Point(val x: Int, val y: Int)
-
   companion object {
     const val FABRIC_SIZE = 1000
-
-    fun doOverlap(l1: Point, r1: Point, l2: Point, r2: Point): Boolean {
-
-      // To check if either rectangle is actually a line
-      // For example :  l1 ={-1,0}  r1={1,1}  l2={0,-1}  r2={0,1}
-      if (l1.x == r1.x || l1.y == r1.y || l2.x == r2.x || l2.y == r2.y) {
-        // the line cannot have positive overlap
-        return false
-      }
-
-      // If one rectangle is on left side of other
-      if (l1.x >= r2.x || l2.x >= r1.x) {
-        return false
-      }
-
-      // If one rectangle is above other
-      return !(r1.y >= l2.y || r2.y >= l1.y)
-    }
   }
 }
