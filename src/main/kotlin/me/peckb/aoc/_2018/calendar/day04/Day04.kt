65b1529e3b088e83e05411325d74afda9ab63372
Brian Peck <peckb@squareup.com>
2022-01-15 17:01:08 -0800

    Day 04 2015

---
+++ src/main/kotlin/me/peckb/aoc/_2018/calendar/day04/Day04.kt
@@ -0,0 +1,85 @@
+package me.peckb.aoc._2018.calendar.day04
+
+import me.peckb.aoc._2018.calendar.day04.Day04.Action.BeginShift
+import me.peckb.aoc._2018.calendar.day04.Day04.Action.FallAsleep
+import me.peckb.aoc._2018.calendar.day04.Day04.Action.WakesUp
+import javax.inject.Inject
+
+import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+typealias GuardId = Int
+typealias MinuteOfTheHour = Int
+typealias NumberOfTimesAsleep = Int
+
+class Day04 @Inject constructor(private val generatorFactory: InputGeneratorFactory) {
+  fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::event) { input ->
+    val guardsSleepTimes = watchGuards(input)
+
+    val sleepyGuard = guardsSleepTimes.maxByOrNull { it.value.values.sum() }!!
+    val sleepyMinute = sleepyGuard.value.maxByOrNull { it.value }
+
+    sleepyGuard.key * (sleepyMinute?.key ?: -1)
+  }
+
+  fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::event) { input ->
+    val guardsSleepTimes = watchGuards(input)
+
+    val sleepyGuard = guardsSleepTimes.maxByOrNull { it.value.values.maxOf { timesAsleep -> timesAsleep } }!!
+    val sleepyMinute = sleepyGuard.value.maxByOrNull { it.value }
+
+    sleepyGuard.key * (sleepyMinute?.key ?: -1)
+  }
+
+  private fun watchGuards(input: Sequence<Event>): MutableMap<GuardId, MutableMap<MinuteOfTheHour, NumberOfTimesAsleep>> {
+    val guardsSleepTimes =
+      mutableMapOf<GuardId, MutableMap<MinuteOfTheHour, NumberOfTimesAsleep>>()
+        .withDefault { mutableMapOf() }
+
+    var currentGuardId = -1
+    var startSleepMinute = -1
+    input.sortedBy { it.eventTime }.forEach { event ->
+      when (event.action) {
+        is BeginShift -> currentGuardId = event.action.guardId
+        FallAsleep -> startSleepMinute = event.eventTime.minute
+        WakesUp -> {
+          (startSleepMinute until event.eventTime.minute).forEach { minute ->
+            val guardSleepTimes = guardsSleepTimes.getValue(currentGuardId)
+            guardSleepTimes.merge(minute, 1, Int::plus)
+            guardsSleepTimes[currentGuardId] = guardSleepTimes
+          }
+        }
+      }
+    }
+
+    return guardsSleepTimes
+  }
+
+  private fun event(line: String): Event {
+    val timeString = line.substringBefore("]").drop(1)
+    val eventTime = LocalDateTime.parse(timeString, formatter)
+
+    val parts = line.split(" ")
+    val action = when (parts[2]) {
+      "Guard" -> BeginShift(parts[3].drop(1).toInt())
+      "falls" -> FallAsleep
+      "wakes" -> WakesUp
+      else -> throw IllegalArgumentException("Unknown Event: $line")
+    }
+
+    return Event(eventTime, action)
+  }
+
+  data class Event(val eventTime: LocalDateTime, val action: Action)
+
+  sealed class Action {
+    data class BeginShift(val guardId: Int) : Action()
+    object FallAsleep : Action()
+    object WakesUp: Action()
+  }
+
+  companion object {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+  }
+}
---
+++ src/test/kotlin/me/peckb/aoc/_2018/calendar/day04/Day04Test.kt
@@ -0,0 +1,33 @@
+package me.peckb.aoc._2018.calendar.day04
+
+import javax.inject.Inject
+
+
+import me.peckb.aoc._2018.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Day04Test {
+  @Inject
+  lateinit var day04: Day04
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Test
+  fun testDay04PartOne() {
+    assertEquals(140932, day04.partOne(DAY_04))
+  }
+
+  @Test
+  fun testDay04PartTwo() {
+    assertEquals(51232, day04.partTwo(DAY_04))
+  }
+
+  companion object {
+    private const val DAY_04: String = "src/test/resources/2018/day04.input"
+  }
+}
