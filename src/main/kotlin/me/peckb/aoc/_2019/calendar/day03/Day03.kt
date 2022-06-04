1d694510aa2ebfbce1480318116084784d9f0538
Brian Peck <peckb@squareup.com>
2022-06-04 12:36:00 -0700

    cleanup

--- src/main/kotlin/me/peckb/aoc/_2019/calendar/day03/Day03.kt
+++ src/main/kotlin/me/peckb/aoc/_2019/calendar/day03/Day03.kt
@@ -9,84 +9,19 @@ class Day03 @Inject constructor(
   private val generatorFactory: InputGeneratorFactory,
 ) {
   fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::directions) { input ->
-    val directionsList = input.toList()
+    val coveredPaths = getConnectionPoints(input)
 
-    val coveredPaths = mutableMapOf<Point, Set<Int>>()
-
-    directionsList.forEachIndexed { index, directions ->
-      var x = 0
-      var y = 0
-
-      directions.forEach {
-        val direction = it[0]
-        val length = it.substring(1).toInt()
-        repeat(length) { zeroIndexedMove ->
-          val move = zeroIndexedMove + 1
-          when (direction) {
-            'R' -> coveredPaths.merge(Point(x + move, y), setOf(index)) { a, b -> a + b }
-            'L' -> coveredPaths.merge(Point(x - move, y), setOf(index)) { a, b -> a + b }
-            'U' -> coveredPaths.merge(Point(x, y - move), setOf(index)) { a, b -> a + b }
-            'D' -> coveredPaths.merge(Point(x, y + move), setOf(index)) { a, b -> a + b }
-          }
-        }
-        when (direction) {
-          'R' -> x += length
-          'L' -> x -= length
-          'U' -> y -= length
-          'D' -> y += length
-        }
-      }
-    }
-
-    val closestConnectionPoint = coveredPaths
-      .filter { it.value.size == 2 }
-      .filterNot { it.key.x == 0 && it.key.y == 0 }
+    coveredPaths
       .map { abs(it.key.x) + abs(it.key.y) }
       .minOrNull()
-
-    closestConnectionPoint
   }
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::directions) { input ->
-    val directionsList = input.toList()
+    val coveredPaths = getConnectionPoints(input)
 
-    val coveredPaths = mutableMapOf<Point, Set<Owner>>()
-
-    directionsList.forEachIndexed { index, directions ->
-      var x = 0
-      var y = 0
-      var stepsTaken = 0
-
-      directions.forEach {
-        val direction = it[0]
-        val length = it.substring(1).toInt()
-        repeat(length) { zeroIndexedMove ->
-          val move = zeroIndexedMove + 1
-          stepsTaken += 1
-          val owner = Owner(index).withSteps(stepsTaken)
-          when (direction) {
-            'R' -> coveredPaths.merge(Point(x + move, y), setOf(owner)) { a, b -> a + b }
-            'L' -> coveredPaths.merge(Point(x - move, y), setOf(owner)) { a, b -> a + b }
-            'U' -> coveredPaths.merge(Point(x, y - move), setOf(owner)) { a, b -> a + b }
-            'D' -> coveredPaths.merge(Point(x, y + move), setOf(owner)) { a, b -> a + b }
-          }
-        }
-        when (direction) {
-          'R' -> x += length
-          'L' -> x -= length
-          'U' -> y -= length
-          'D' -> y += length
-        }
-      }
-    }
-
-    val firstConnectionPoint = coveredPaths
-      .filter { it.value.size == 2 }
-      .filterNot { it.key.x == 0 && it.key.y == 0 }
+    coveredPaths
       .map { it.value.sumOf { owner -> owner.stepsTaken } }
       .minOrNull()
-
-    firstConnectionPoint
   }
 
   private fun directions(line: String) = line.split(",")
@@ -98,5 +33,46 @@ class Day03 @Inject constructor(
       private set
 
     fun withSteps(stepsTaken: Int) = apply { this.stepsTaken = stepsTaken }
+  }
+
+  private fun getConnectionPoints(directionsList: Sequence<List<String>>): Map<Point, Set<Owner>> {
+    val coveredPaths = mutableMapOf<Point, Set<Owner>>().apply {}
+
+    directionsList.forEachIndexed { index, directions ->
+      var x = 0
+      var y = 0
+      var stepsTaken = 0
+
+      directions.forEach {
+        val direction = it[0]
+        val length = it.substring(1).toInt()
+
+        repeat(length) { zeroIndexedMove ->
+          val move = zeroIndexedMove + 1
+          stepsTaken += 1
+
+          val point = when (direction) {
+            'R' -> Point(x + move, y)
+            'L' -> Point(x - move, y)
+            'U' -> Point(x, y - move)
+            'D' -> Point(x, y + move)
+            else -> Point(x, y)
+          }
+
+          val ownerSet = setOf(Owner(index).withSteps(stepsTaken))
+          coveredPaths.merge(point, ownerSet) { a, b -> a + b }
+        }
+        when (direction) {
+          'R' -> x += length
+          'L' -> x -= length
+          'U' -> y -= length
+          'D' -> y += length
+        }
+      }
+    }
+
+    return coveredPaths
+      .filter { it.value.size == 2 }
+      .filterNot { it.key.x == 0 && it.key.y == 0 }
   }
 }
