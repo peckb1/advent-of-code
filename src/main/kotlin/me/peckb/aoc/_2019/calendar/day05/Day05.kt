8f6b28f0927d9aee5c4be3c3cb926addba496072
Brian Peck <peckb@squareup.com>
2022-10-24 22:29:16 -0700

    Cleanup

--- src/main/kotlin/me/peckb/aoc/_2019/calendar/day05/Day05.kt
+++ src/main/kotlin/me/peckb/aoc/_2019/calendar/day05/Day05.kt
@@ -29,158 +29,73 @@ class Day05 @Inject constructor(
     var pointerIndex = 0
     while(operations[pointerIndex].toInt() != HALT_PROGRAM) {
       val codeString = operations[pointerIndex].padStart(5, '0')
-      val thirdParameterMode = codeString[0].digitToInt()
-      val secondParameterMode = codeString[1].digitToInt()
-      val firstParameterMode = codeString[2].digitToInt()
-      val code = codeString.takeLast(2).toInt()
 
-      when (code) {
+      @Suppress("UNUSED_VARIABLE") val thirdParameterMode = codeString[0].digitToInt()
+      val secondParameterMode by lazy { codeString[1].digitToInt() }
+      val firstParameterMode by lazy { codeString[2].digitToInt() }
+
+      val firstParameterValue by lazy { operations[pointerIndex + 1].toInt() }
+      val secondParameterValue by lazy { operations[pointerIndex + 2].toInt() }
+      val thirdParameterValue by lazy { operations[pointerIndex + 3].toInt() }
+
+      val aValue by lazy {
+        when (firstParameterMode) {
+          IMMEDIATE_MODE -> firstParameterValue
+          POSITION_MODE -> operations[firstParameterValue].toInt()
+          else -> throw IllegalArgumentException("Invalid Mode")
+        }.toInt()
+      }
+      val bValue by lazy {
+        when (secondParameterMode) {
+          IMMEDIATE_MODE -> secondParameterValue
+          POSITION_MODE -> operations[secondParameterValue].toInt()
+          else -> throw IllegalArgumentException("Invalid Mode")
+        }.toInt()
+      }
+
+      when (codeString.takeLast(2).toInt()) {
         ADD -> {
-          val firstParameterValue = operations[pointerIndex + 1].toInt()
-          val secondParameterValue = operations[pointerIndex + 2].toInt()
-          val thirdParameterValue = operations[pointerIndex + 3].toInt()
-
-          val aValue = when (firstParameterMode) {
-            IMMEDIATE_MODE -> firstParameterValue
-            POSITION_MODE -> operations[firstParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-          val bValue = when (secondParameterMode) {
-            IMMEDIATE_MODE -> secondParameterValue
-            POSITION_MODE -> operations[secondParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-
           operations[thirdParameterValue] = (aValue + bValue).toString()
-
           pointerIndex += 4
         }
         MUL -> {
-          val firstParameterValue = operations[pointerIndex + 1].toInt()
-          val secondParameterValue = operations[pointerIndex + 2].toInt()
-          val thirdParameterValue = operations[pointerIndex + 3].toInt()
-
-          val aValue = when (firstParameterMode) {
-            IMMEDIATE_MODE -> firstParameterValue
-            POSITION_MODE -> operations[firstParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-          val bValue = when (secondParameterMode) {
-            IMMEDIATE_MODE -> secondParameterValue
-            POSITION_MODE -> operations[secondParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-
           operations[thirdParameterValue] = (aValue * bValue).toString()
-
           pointerIndex += 4
         }
         IN -> {
           val input = userInput().toString()
-          val parameterValue = operations[pointerIndex + 1].toInt()
-
-          operations[parameterValue] = input
-
+          operations[firstParameterValue] = input
           pointerIndex += 2
         }
         OUT -> {
-          val parameterValue = operations[pointerIndex + 1].toInt()
-          val registerValue = operations[parameterValue].toInt()
-
           when (firstParameterMode) {
-            IMMEDIATE_MODE -> handleOutput(parameterValue)
-            POSITION_MODE -> handleOutput(registerValue)
+            IMMEDIATE_MODE -> handleOutput(firstParameterValue)
+            POSITION_MODE -> handleOutput(operations[firstParameterValue].toInt())
           }
           pointerIndex += 2
         }
         JUMP_IF_TRUE -> {
-          val firstParameterValue = operations[pointerIndex + 1].toInt()
-          val secondParameterValue = operations[pointerIndex + 2].toInt()
-
-          val aValue = when (firstParameterMode) {
-            IMMEDIATE_MODE -> firstParameterValue
-            POSITION_MODE -> operations[firstParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-
           if (aValue != 0) {
-            val bValue = when (secondParameterMode) {
-              IMMEDIATE_MODE -> secondParameterValue
-              POSITION_MODE -> operations[secondParameterValue].toInt()
-              else -> throw IllegalArgumentException("Invalid Mode")
-            }.toInt()
-
             pointerIndex = bValue
           } else {
             pointerIndex += 3
           }
         }
         JUMP_IF_FALSE -> {
-          val firstParameterValue = operations[pointerIndex + 1].toInt()
-          val secondParameterValue = operations[pointerIndex + 2].toInt()
-
-          val aValue = when (firstParameterMode) {
-            IMMEDIATE_MODE -> firstParameterValue
-            POSITION_MODE -> operations[firstParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-
           if (aValue == 0) {
-            val bValue = when (secondParameterMode) {
-              IMMEDIATE_MODE -> secondParameterValue
-              POSITION_MODE -> operations[secondParameterValue].toInt()
-              else -> throw IllegalArgumentException("Invalid Mode")
-            }.toInt()
-
             pointerIndex = bValue
           } else {
             pointerIndex += 3
           }
         }
         LESS_THAN -> {
-          val firstParameterValue = operations[pointerIndex + 1].toInt()
-          val secondParameterValue = operations[pointerIndex + 2].toInt()
-          val thirdParameterValue = operations[pointerIndex + 3].toInt()
-
-          val aValue = when (firstParameterMode) {
-            IMMEDIATE_MODE -> firstParameterValue
-            POSITION_MODE -> operations[firstParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-
-          val bValue = when (secondParameterMode) {
-            IMMEDIATE_MODE -> secondParameterValue
-            POSITION_MODE -> operations[secondParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-
           val valueToSet = if (aValue < bValue) { 1 } else { 0 }
-
           operations[thirdParameterValue] = valueToSet.toString()
-
           pointerIndex += 4
         }
         EQUALS -> {
-          val firstParameterValue = operations[pointerIndex + 1].toInt()
-          val secondParameterValue = operations[pointerIndex + 2].toInt()
-          val thirdParameterValue = operations[pointerIndex + 3].toInt()
-
-          val aValue = when (firstParameterMode) {
-            IMMEDIATE_MODE -> firstParameterValue
-            POSITION_MODE -> operations[firstParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-
-          val bValue = when (secondParameterMode) {
-            IMMEDIATE_MODE -> secondParameterValue
-            POSITION_MODE -> operations[secondParameterValue].toInt()
-            else -> throw IllegalArgumentException("Invalid Mode")
-          }.toInt()
-
           val valueToSet = if (aValue == bValue) { 1 } else { 0 }
-
           operations[thirdParameterValue] = valueToSet.toString()
-
           pointerIndex += 4
         }
       }
