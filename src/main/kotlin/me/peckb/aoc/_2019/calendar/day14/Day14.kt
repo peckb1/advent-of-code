cd25c13ab8f46155e031d876f43ef85a7190262a
Brian Peck <peckb@squareup.com>
2022-11-12 21:21:02 -0800

    Optimization 1. 4 min down to 2.5 min

--- src/main/kotlin/me/peckb/aoc/_2019/calendar/day14/Day14.kt
+++ src/main/kotlin/me/peckb/aoc/_2019/calendar/day14/Day14.kt
@@ -54,16 +54,12 @@ class Day14 @Inject constructor(
   ): Long {
     var oreMined = 0L
     val chemicalsNeeded = mutableListOf<ChemicalAmount>().also { it.add(1 to FUEL)  }
-//    val surplusChemicals = mutableMapOf<Chemical, Amount>().withDefault { 0 }
 
     while(chemicalsNeeded.isNotEmpty()) {
       // what do we need to make?
-//      println("NEED: $chemicalsNeeded")
-//      println("HAVE: $surplusChemicals")
       val (amountOfNeededChemical, neededChemical) = chemicalsNeeded.removeFirst()
 
       val amountInSurplus = surplusChemicals.getValue(neededChemical)
-//      println("\t There is $amountInSurplus $neededChemical in surplus")
 
       // check if we have _any_ of it in storage
       val chemicalsInStorageWeCanUse = if (amountInSurplus > 0) {
@@ -75,9 +71,7 @@ class Day14 @Inject constructor(
       if (chemicalsInStorageWeCanUse >= amountOfNeededChemical) {
         // if we have an abundance in storage, just use our surplus
         surplusChemicals[neededChemical] = amountInSurplus - amountOfNeededChemical
-//        println("\t We can use all $chemicalsInStorageWeCanUse $neededChemical from storage")
       } else {
-//        println("\t We can use $chemicalsInStorageWeCanUse $neededChemical from storage")
         // we don't have enough in storage to cover our needs, so let's make some!
         val reactionToMakeNeededChemical = reactions.getValue(neededChemical)
         val amountOfChemicalMadeFromReaction = reactionToMakeNeededChemical.output.first
@@ -90,43 +84,27 @@ class Day14 @Inject constructor(
         }
         val amountOfChemicalCreatedFromReaction = numberOfReactionsNeeded * amountOfChemicalMadeFromReaction
 
-//        println("\t We need $numberOfReactionsNeeded reaction(s) of $reactionToMakeNeededChemical")
-//        println("\t This will make $amountOfChemicalCreatedFromReaction $neededChemical to cover our $amountOfNeededChemical $neededChemical")
-//        if (extraChemicalsWouldBeMade) {
-//          println("\t We will end up ${amountOfChemicalCreatedFromReaction - amountOfNeededChemical} $neededChemical that need to be stored")
-//        } else {
-//          println("\t We will not have to store any extra chemicals, but should cleanup the $chemicalsInStorageWeCanUse we borrowed")
-//        }
-
         reactionToMakeNeededChemical.input.forEach { (reactionInputAmount, reactionInputChemical) ->
           val amountOfInputNeededForReaction = numberOfReactionsNeeded * reactionInputAmount
-//          println("\t\t We need $amountOfInputNeededForReaction $reactionInputChemical for the $numberOfReactionsNeeded reactions needed")
-//          if (extraChemicalsWouldBeMade) {
-//            val surplusChemicalsThatWouldBeCreated = amountOfChemicalCreatedFromReaction - amountOfNeededChemical
-//            surplusChemicals[neededChemical] = amountInSurplus + surplusChemicalsThatWouldBeCreated
-////            println("\t\t We would create an extra $surplusChemicalsThatWouldBeCreated $neededChemical")
-////            println("\t\t Our new total Surplus is ${surplusChemicals[neededChemical]}")
-//          } else {
-//            surplusChemicals[neededChemical] = amountInSurplus - chemicalsInStorageWeCanUse
-////            println("\t\t Taking the $chemicalsInStorageWeCanUse out of storage for a new total of ${surplusChemicals[neededChemical]}")
-//          }
           if (reactionInputChemical == ORE) {
             oreMined += amountOfInputNeededForReaction
-//            println("\t\t\t Mining $amountOfInputNeededForReaction more order for $oreMined total")
           } else {
-//            println("\t\t\t Adding to backlog")
-            chemicalsNeeded.add(amountOfInputNeededForReaction to reactionInputChemical)
+            val indexOfExistingNeed = chemicalsNeeded.indexOfFirst { (_, chemical) ->
+              chemical == reactionInputChemical
+            }
+            if (indexOfExistingNeed == -1) {
+              chemicalsNeeded.add(amountOfInputNeededForReaction to reactionInputChemical)
+            } else {
+              chemicalsNeeded[indexOfExistingNeed] = (chemicalsNeeded[indexOfExistingNeed].first + amountOfInputNeededForReaction) to reactionInputChemical
+            }
           }
         }
 
         if (extraChemicalsWouldBeMade) {
           val surplusChemicalsThatWouldBeCreated = amountOfChemicalCreatedFromReaction - amountOfNeededChemical
           surplusChemicals[neededChemical] = amountInSurplus + surplusChemicalsThatWouldBeCreated
-//            println("\t\t We would create an extra $surplusChemicalsThatWouldBeCreated $neededChemical")
-//            println("\t\t Our new total Surplus is ${surplusChemicals[neededChemical]}")
         } else {
           surplusChemicals[neededChemical] = amountInSurplus - chemicalsInStorageWeCanUse
-//            println("\t\t Taking the $chemicalsInStorageWeCanUse out of storage for a new total of ${surplusChemicals[neededChemical]}")
         }
       }
     }
@@ -135,7 +113,6 @@ class Day14 @Inject constructor(
   }
 
   private fun reaction(line: String): Reaction {
-    // 7 A, 1 C => 1 D
     val (inputStr, outputStr) = line.split(" => ")
     val inputChemicalAmounts = inputStr.split(", ")
       .map { inputData -> inputData.toChemicalAmount() }
