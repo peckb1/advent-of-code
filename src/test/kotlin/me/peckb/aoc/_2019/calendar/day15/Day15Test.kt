9d051c4fb79df36db86d5db1a0ce843c4af93167
Brian Peck <peckb@squareup.com>
2022-11-13 20:59:53 -0800

    Day 15 2019

---
+++ src/test/kotlin/me/peckb/aoc/_2019/calendar/day15/Day15Test.kt
@@ -0,0 +1,33 @@
+package me.peckb.aoc._2019.calendar.day15
+
+import javax.inject.Inject
+
+
+import me.peckb.aoc._2019.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Day15Test {
+  @Inject
+  lateinit var day15: Day15
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Test
+  fun testDay15PartOne() {
+    assertEquals(212, day15.partOne(DAY_15))
+  }
+
+  @Test
+  fun testDay15PartTwo() {
+    assertEquals(358, day15.partTwo(DAY_15))
+  }
+
+  companion object {
+    private const val DAY_15: String = "src/test/resources/2019/day15.input"
+  }
+}
