34d1c94ffc500a0713c071ae4b4ae50d542161c4
Brian Peck <peckb@squareup.com>
2022-11-15 22:49:19 -0800

    WIP 2, better, but should bring back the early leave if cost too high piece

--- src/main/kotlin/me/peckb/aoc/_2019/calendar/day18/Day18.kt
+++ src/main/kotlin/me/peckb/aoc/_2019/calendar/day18/Day18.kt
@@ -4,117 +4,183 @@ import me.peckb.aoc._2019.calendar.day18.Day18.Section.*
 import javax.inject.Inject
 
 import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
-import me.peckb.aoc.pathing.GenericIntDijkstra
-import me.peckb.aoc.pathing.GenericIntDijkstra.DijkstraNode
+import me.peckb.aoc.pathing.Dijkstra
+import me.peckb.aoc.pathing.DijkstraNodeWithCost
 import java.lang.IllegalArgumentException
-import kotlin.math.min
-
-typealias Cost = Int
 
 class Day18 @Inject constructor(
   private val generatorFactory: InputGeneratorFactory,
 ) {
+  class CaveDijkstra(private val caves: List<List<Section>>) : Dijkstra<Area, Path, AreaWithPath> {
+    override fun Path.plus(cost: Path): Path = cost
+
+    override fun Area.withCost(cost: Path): AreaWithPath = AreaWithPath(this, cost).withCaves(caves)
+
+    override fun minCost(): Path = Path(emptyList(), Int.MIN_VALUE)
+
+    override fun maxCost(): Path = Path(emptyList(), Int.MAX_VALUE)
+  }
+
+  class AreaWithPath(private val area: Area, private val path: Path) : DijkstraNodeWithCost<Area, Path> {
+    private lateinit var caves: List<List<Section>>
+
+    override fun compareTo(other: DijkstraNodeWithCost<Area, Path>): Int {
+      val pathCompare by lazy { path.compareTo(other.cost()) }
+      val yCompare by lazy { path.steps.last().y.compareTo(other.cost().steps.last().y) }
+      val xCompare by lazy { path.steps.last().x.compareTo(other.cost().steps.last().x) }
+
+      return when (pathCompare) {
+        0 -> {
+          when (yCompare) {
+            0 -> xCompare
+            else -> yCompare
+          }
+        }
+        else -> pathCompare
+      }
+    }
+
+    override fun neighbors(): List<AreaWithPath> {
+      val (x, y) = area
+
+      val n = caves[y - 1][x] to Area(x, y - 1)
+      val e = caves[y][x + 1] to Area(x + 1, y)
+      val s = caves[y + 1][x] to Area(x, y + 1)
+      val w = caves[y][x - 1] to Area(x - 1, y)
+
+      return listOf(n, e, s, w)
+        .filter { (section, _) -> section !is WALL }
+        .map { (_, area) -> AreaWithPath(area, Path(path.steps.plus(area))) }
+    }
+
+    override fun node(): Area = area
+
+    override fun cost(): Path = path
+
+    fun withCaves(caves: List<List<Section>>) = apply {
+      this.caves = caves
+    }
+  }
+
+  data class Area(val x: Int, val y: Int)
+
+  data class Path(val steps: List<Area>, val cost: Int = steps.size): Comparable<Path> {
+    override fun compareTo(other: Path) = cost.compareTo(other.cost)
+  }
+
   fun partOne(filename: String) = generatorFactory.forFile(filename).read { input ->
-    val (caves, startingLocation) = createCaves(input)
-    caves.print()
-    caves[startingLocation.y][startingLocation.x] = EMPTY
-
-    val maxKeysToFind = caves.howManyKeys()
-
-    val randomInitialCost = findRandomPathCost(caves, startingLocation)
-    findReachableKeysCost(
-      caves = caves,
-      myLocation = startingLocation,
-      foundKeys = emptySet(),
-      costNotToExceed = randomInitialCost,
-      costSoFar = 0,
-      maxKeysToFind = maxKeysToFind
-    )
-  }
-
-  private fun findRandomPathCost(
-    caves: List<List<Section>>,
-    startLocation: Location
-  ): Int {
-    var runningCost = 0
-    val foundKeys = mutableSetOf<KEY>()
-    var firstReachableKeys = searchForKeys(caves, startLocation, foundKeys)
-
-    while(firstReachableKeys.isNotEmpty()) {
-      val (key, location, cost) = firstReachableKeys.random()
-      runningCost += cost
-      foundKeys.add(key)
-      firstReachableKeys = searchForKeys(caves, location, foundKeys)
+    // parse our input and create our caves
+    val (caves, startingLocation) = createCaves(input).also { (c, loc) ->
+      c.print()
+      c[loc.y][loc.x] = EMPTY
     }
 
-    return runningCost
-  }
-
-  private fun findReachableKeysCost(
-    caves: List<List<Section>>,
-    myLocation: Location,
-    foundKeys: Set<KEY>,
-    costNotToExceed: Int,
-    costSoFar: Int,
-    maxKeysToFind: Int
-  ): Int {
-    val firstReachableKeys = searchForKeys(caves, myLocation, foundKeys)
-
-    if (firstReachableKeys.isEmpty()) {
-      return 0
+    // find our keys!
+    val keys = caves.flatMapIndexed { y, row ->
+      row.mapIndexedNotNull { x, section ->
+        if (section is KEY) { section to Area(x, y) } else { null }
+      }
     }
 
-    var myCheapestChild = 9000 - costSoFar
-    var newHighCost = costNotToExceed
-    val myCheapestChildCost = firstReachableKeys.minOf search@ { (key, location, cost) ->
-      if (costSoFar + cost > newHighCost) {
-        return@search 9000
-      }
-
-      val newKeySet = foundKeys.plus(key)
-      println("\t".repeat(foundKeys.size) + "${newKeySet.size} $key")
-      if (newKeySet.size == maxKeysToFind) {
-        return 0
-      }
-
-
-      val cheapestCostAfterFindingKey = findReachableKeysCost(
-        caves = caves,
-        myLocation = location,
-        foundKeys = newKeySet,
-        costNotToExceed = newHighCost,
-        costSoFar = costSoFar + cost,
-        maxKeysToFind = maxKeysToFind
+    // setup a map of every key to the path to everywhere else on the map
+    val solver = CaveDijkstra(caves)
+    val routes = mutableMapOf<KEY, MutableMap<Area, Path>>().apply {
+      keys.associateByTo(
+        destination = this,
+        keySelector = { (key, _) -> key},
+        valueTransform = { (_, area) -> solver.solve(area) }
       )
-
-      myCheapestChild = min(cheapestCostAfterFindingKey, myCheapestChild)
-      newHighCost = min(costNotToExceed, costSoFar + myCheapestChild)
-
-      cost + cheapestCostAfterFindingKey
     }
 
-    return myCheapestChildCost
+    // create some helper/reference maps
+    val keysByLocation = keys.associateBy({ it.second }, { it.first })
+    val locationsByKeys = keys.associateBy({ it.first }, { it.second })
+
+    // cull the map of keys to everywhere to just a map of keys to other keys
+    // convert "routes" into just the paths from key to key, knowing which doors block those keys
+    val keyToKeyPaths = routes.mapValues { (key, paths) ->
+      paths.filter { (area, _) -> keysByLocation.contains(area) }
+        .filterNot { (area, _) -> keysByLocation.getValue(area) == key }
+        .mapKeys { (area, _) -> keysByLocation.getValue(area) }
+    }
+
+    // find the keys reachable from our start location
+    val keysFromStart = solver.solve(startingLocation).filter { (area, path) ->
+      keysByLocation.contains(area)
+    }.filterNot { (_, path) ->
+      path.steps.any { area -> caves[area.y][area.x] is DOOR }
+    }
+
+    findCheapPath(keysFromStart, keyToKeyPaths, keysByLocation, locationsByKeys, caves)
   }
 
-  private fun searchForKeys(
+  private fun findCheapPath(
+    keysFromStart: Map<Area, Path>,
+    keyToKeyPaths: Map<KEY, Map<KEY, Path>>,
+    keysByLocation: Map<Area, KEY>,
+    locationsByKeys: Map<KEY, Area>,
+    caves: List<List<Section>>
+  ): Int? {
+    return keysFromStart.mapNotNull { (startKeyArea, pathToStartKey) ->
+      println("Start Key ${keysByLocation[startKeyArea]}")
+
+      findRoute(
+        keyToKeyPaths,
+        keysByLocation,
+        locationsByKeys,
+        caves,
+        startKeyArea,
+        setOf(keysByLocation.getValue(startKeyArea)),
+        pathToStartKey.cost
+      )
+    }.minOfOrNull { it }
+  }
+
+  private fun findRoute(
+    keyToKeyPaths: Map<KEY, Map<KEY, Path>>,
+    keysByLocation: Map<Area, KEY>,
+    locationsByKeys: Map<KEY, Area>,
     caves: List<List<Section>>,
-    currentLocation: Location,
-    keysFound: Set<KEY>
-  ): List<AttainableKeys> {
-    val keyLocations = mutableSetOf<Pair<KEY, Location>>()
-
-    val searcher = object : GenericIntDijkstra<Location>() {}
-
-    val start = Location(currentLocation.x, currentLocation.y)
-      .withCaves(caves)
-      .withKeysFound(keysFound)
-      .withKeyLocations(keyLocations)
-    val paths = searcher.solve(start)
-
-    return keyLocations.mapNotNull { (key, loc) ->
-      paths[Location(loc.x, loc.y)]?.let { cost ->
-        AttainableKeys(key, loc, cost)
+    startArea: Area,
+    foundKeys: Set<KEY>,
+    costToHere: Int
+  ): Int? {
+    val myKey = keysByLocation.getValue(startArea)
+    val reachableKeys = keyToKeyPaths.getValue(myKey)
+      .filterNot { (key, _) -> foundKeys.contains(key) }
+      .filterNot { (_, path) ->
+        path.steps.any { area ->
+          val section = caves[area.y][area.x]
+          section is DOOR && !foundKeys.contains(KEY(section.identifier.lowercaseChar()))
+        }
       }
+
+    return if (reachableKeys.isEmpty()) {
+      // we have nowhere to go!
+      if (foundKeys.size == keyToKeyPaths.size) {
+        // we found all possible keys!
+        println("$costToHere $foundKeys")
+        costToHere
+      } else {
+        // we have nowhere to go, but didn't find everyone, so it's a failed route
+        null
+      }
+    } else {
+      // we have more keys to try and fetch!
+      reachableKeys.mapNotNull { (nextKey, pathToNextKey) ->
+        if (foundKeys.size < 5) {
+          println("\t".repeat(foundKeys.size) +"NextKey Key $nextKey")
+        }
+        findRoute(
+          keyToKeyPaths,
+          keysByLocation,
+          locationsByKeys,
+          caves,
+          locationsByKeys.getValue(nextKey),
+          foundKeys.plus(nextKey),
+          costToHere + pathToNextKey.cost
+        )
+      }.minOfOrNull { it }
     }
   }
 
@@ -122,69 +188,17 @@ class Day18 @Inject constructor(
     -1
   }
 
-  data class Location(val x: Int, val y: Int) : DijkstraNode<Location> {
-    private lateinit var caves: List<List<Section>>
-    private lateinit var keysFound: Set<KEY>
-    private lateinit var keyLocations: MutableSet<Pair<KEY, Location>>
 
-    fun withCaves(caves: List<List<Section>> = mutableListOf()) = apply {
-      this.caves = caves
-    }
-
-    fun withKeysFound(keysFound: Set<KEY>) = apply {
-      this.keysFound = keysFound
-    }
-
-    fun withKeyLocations(keyLocations: MutableSet<Pair<KEY, Location>>) = apply {
-      this.keyLocations = keyLocations
-    }
-
-    override fun neighbors(): Map<Location, Int> {
-      val mySpot = caves[y][x]
-      if (mySpot is KEY) {
-        // if we have not found this key before
-        if (!keysFound.contains(mySpot)) {
-          // store it as a new key location
-          keyLocations.add(mySpot to Location(x, y).withCaves(caves).withKeysFound(keysFound).withKeyLocations(keyLocations))
-          // and don't explore further from this spot
-          return emptyMap()
-        }
-      }
-
-      // if we did not return early from finding a key, check our neighbors
-      val n = caves[y - 1][x] to Location(x, y - 1).withCaves(caves).withKeysFound(keysFound).withKeyLocations(keyLocations)
-      val e = caves[y][x + 1] to Location(x + 1, y).withCaves(caves).withKeysFound(keysFound).withKeyLocations(keyLocations)
-      val s = caves[y + 1][x] to Location(x, y + 1).withCaves(caves).withKeysFound(keysFound).withKeyLocations(keyLocations)
-      val w = caves[y][x - 1] to Location(x - 1, y).withCaves(caves).withKeysFound(keysFound).withKeyLocations(keyLocations)
-
-      val neighborsToExplore = listOf(n, e, s, w).filter { (section, _) ->
-        when (section) {
-          is DOOR -> {
-            keysFound.contains(KEY(section.identifier.lowercaseChar()))
-          }
-          is KEY -> true
-          EMPTY -> true
-          PERSON -> true
-          WALL -> false
-        }
-      }
-
-      return neighborsToExplore.map { it.second }.associateWith { 1 }
-    }
-  }
-
-  data class AttainableKeys(val key: KEY, val location: Location, val cost: Cost)
-
-  private fun createCaves(input: Sequence<String>): Pair<MutableList<MutableList<Section>>, Location> {
+  private fun createCaves(input: Sequence<String>): Pair<MutableList<MutableList<Section>>, Area> {
     val caves = mutableListOf<MutableList<Section>>()
-    var location = Location(-1, -1)
+    var location = Area(-1, -1)
 
     input.forEachIndexed { y, line ->
       val row = mutableListOf<Section>()
       line.forEachIndexed { x, sectionChar ->
         val section = Section.fromChar(sectionChar)
         row.add(section).also {
-          if (section is PERSON) location = Location(x, y)
+          if (section is PERSON) location = Area(x, y)
         }
       }
       caves.add(row)
@@ -196,15 +210,6 @@ class Day18 @Inject constructor(
     forEach { row ->
       row.forEach { print(it) }
       println()
-    }
-  }
-
-  private fun List<List<Section>>.howManyKeys(): Int {
-    return this.sumOf { row ->
-      row.sumOf { section ->
-        val result: Int = if (section is KEY) 1 else 0
-        result
-      }
     }
   }
 
