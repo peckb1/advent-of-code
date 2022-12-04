cb56712fc4c1bbe688a28f95f6eec0fac248c55e
Brian Peck <Catbert321@gmail.com>
2022-12-03 22:09:39 -0800

    Day 04 2022 (#159)
    * Day 04 2022
    
    * readme
    
    * quick nameing update

---
+++ src/test/kotlin/me/peckb/aoc/_2022/calendar/day04/Day04Test.kt
@@ -0,0 +1,33 @@
+package me.peckb.aoc._2022.calendar.day04
+
+import javax.inject.Inject
+
+
+import me.peckb.aoc._2022.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Day04Test {
+  @Inject
+  lateinit var day04: Day04
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Test
+  fun testDay04PartOne() {
+    assertEquals(477, day04.partOne(DAY_04))
+  }
+
+  @Test
+  fun testDay04PartTwo() {
+    assertEquals(830, day04.partTwo(DAY_04))
+  }
+
+  companion object {
+    private const val DAY_04: String = "src/test/resources/2022/day04.input"
+  }
+}
