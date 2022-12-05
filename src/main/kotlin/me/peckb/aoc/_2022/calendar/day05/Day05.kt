5b3a78643811e8594958d7678fea50da388b5090
Brian Peck <Catbert321@gmail.com>
2022-12-05 09:05:05 -0800

    Day 05 2022 (#160)

---
+++ src/main/kotlin/me/peckb/aoc/_2022/calendar/day05/Day05.kt
@@ -0,0 +1,57 @@
+package me.peckb.aoc._2022.calendar.day05
+
+import javax.inject.Inject
+
+import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
+
+class Day05 @Inject constructor(
+  private val generatorFactory: InputGeneratorFactory,
+) {
+  fun partOne(filename: String) = generatorFactory.forFile(filename).read { input ->
+    movePackages(input)
+  }
+
+  fun partTwo(filename: String) = generatorFactory.forFile(filename).read { input ->
+    movePackages(input, stackModifierFunction = List<Char>::reversed)
+  }
+
+  private fun movePackages(
+    input: Sequence<String>,
+    stackModifierFunction: ((List<Char>) -> List<Char>) = { it }
+  ): String {
+    val inputList = input.toList()
+    val (stackCountRowIndex, stacks) = generateStacks(inputList)
+
+    ((stackCountRowIndex + 2) until inputList.size).forEach { inputListIndex ->
+      val moveInstruction = inputList[inputListIndex]
+      val parts = moveInstruction.split(" ")
+      val numToMove = parts[1].toInt()
+      val sourceIndex = parts[3].toInt() - 1
+      val destinationIndex = parts[5].toInt() - 1
+
+      val stacksToMove = (0 until numToMove).map { stacks[sourceIndex].removeFirst() }
+      stackModifierFunction(stacksToMove).forEach { stacks[destinationIndex].addFirst(it) }
+    }
+
+    return stacks.map { it.first() }.joinToString("")
+  }
+
+  private fun generateStacks(inputList: List<String>): Pair<Int, Array<ArrayDeque<Char>>> {
+    val stackCountRow = inputList.find { it[1] == '1' }!!
+    val stackCountRowIndex = inputList.indexOf(stackCountRow)
+    val stackCounts = stackCountRow.trim().split(" ").filterNot { it.isEmpty() }.size
+
+    val stacks = Array(stackCounts) { ArrayDeque<Char>() }
+    repeat(stackCountRowIndex) { row ->
+      val stackData = inputList[row]
+      repeat(stackCounts) { stackId ->
+        val index = (stackId * 4) + 1
+        if (index < stackData.length) {
+          stackData[index].takeUnless { it == ' ' }?.let { stacks[stackId].addLast(it) }
+        }
+      }
+    }
+
+    return stackCountRowIndex to stacks
+  }
+}
---
+++ src/test/kotlin/me/peckb/aoc/_2022/calendar/day05/Day05Test.kt
@@ -0,0 +1,33 @@
+package me.peckb.aoc._2022.calendar.day05
+
+import javax.inject.Inject
+
+
+import me.peckb.aoc._2022.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Day05Test {
+  @Inject
+  lateinit var day05: Day05
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Test
+  fun testDay05PartOne() {
+    assertEquals("SHQWSRBDL", day05.partOne(DAY_05))
+  }
+
+  @Test
+  fun testDay05PartTwo() {
+    assertEquals("CDTQZHBRS", day05.partTwo(DAY_05))
+  }
+
+  companion object {
+    private const val DAY_05: String = "src/test/resources/2022/day05.input"
+  }
+}
