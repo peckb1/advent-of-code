263dc36c876f10d66bbd0d2719e2c07aeedc8bb1
Brian Peck <Catbert321@gmail.com>
2022-12-06 22:41:49 -0800

    Day 07 2022 (#163)
    * initial implementation
    
    * minor refactor
    
    * comment
    
    * Readme

---
+++ src/main/kotlin/me/peckb/aoc/_2022/calendar/day07/Day07.kt
@@ -0,0 +1,87 @@
+package me.peckb.aoc._2022.calendar.day07
+
+import javax.inject.Inject
+
+import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
+import java.lang.IllegalStateException
+
+class Day07 @Inject constructor(
+  private val generatorFactory: InputGeneratorFactory,
+) {
+  fun partOne(filename: String) = generatorFactory.forFile(filename).read { input ->
+    val allDirectories = scanCommands(input.toList())
+    allDirectories.filter { it.size <= 100_000 }.sumOf { it.size }
+  }
+
+  fun partTwo(filename: String) = generatorFactory.forFile(filename).read { input ->
+    val allDirectories = scanCommands(input.toList())
+
+    val totalAllowed = 70_000_000
+    val needFree = 30_000_000
+
+    val currentFree = totalAllowed - allDirectories.first { it.id == "/" }.size
+    val freeSpaceNeeded = needFree - currentFree
+
+    allDirectories.filter { it.size > freeSpaceNeeded }.minOf { it.size }
+  }
+
+  private fun scanCommands(input: List<String>): MutableSet<Directory> {
+    val rootDirectory = Directory("/", null)
+    val allDirectories = mutableSetOf<Directory>().also { it.add(rootDirectory) }
+
+    var currentDirectory = rootDirectory
+    var index = 1 // we can skip the first line that just goes into the root
+
+    while(index < input.size) {
+      val lineParts = input[index].split(" ")
+      val command = lineParts[1]
+      val location by lazy { lineParts[2] }
+
+      when (command) {
+        "ls" -> {
+          var itemsInDirectory = 0
+          var nextIndex = index + 1
+          while (nextIndex < input.size && input[nextIndex][0] != '$') {
+            val item = input[nextIndex]
+            item.split(" ").let { (a, b) ->
+              when (a) {
+                "dir" -> {
+                  val directory = Directory(b, currentDirectory)
+                  allDirectories.add(directory)
+                  currentDirectory.directories.putIfAbsent(b, directory)
+                }
+                else -> currentDirectory.files.putIfAbsent(b, File(b, a.toLong()))
+              }
+            }
+            itemsInDirectory++
+            nextIndex++
+          }
+          index += itemsInDirectory
+        }
+        "cd" -> {
+          currentDirectory = when (location) {
+            "/" -> rootDirectory
+            ".." -> currentDirectory.parent!!
+            else -> currentDirectory.directories[location]!!
+          }
+        }
+        else -> throw IllegalStateException("Unknown Command $location")
+      }
+
+      index++
+    }
+
+    return allDirectories
+  }
+
+  data class File(val name: String, val size: Long)
+
+  data class Directory(val id: String, val parent: Directory?) {
+    val files = mutableMapOf<String, File>()
+    val directories = mutableMapOf<String, Directory>()
+
+    val size: Long by lazy {
+      files.values.sumOf { it.size } + directories.values.sumOf { it.size }
+    }
+  }
+}
