cfa601b096256345b73beeac85a9ca8bd1c290bd
Brian Peck <peckb@squareup.com>
2022-12-08 21:59:36 -0800

    initial reuse refactor

--- src/main/kotlin/me/peckb/aoc/_2022/calendar/day09/Day09.kt
+++ src/main/kotlin/me/peckb/aoc/_2022/calendar/day09/Day09.kt
@@ -1,5 +1,6 @@
 package me.peckb.aoc._2022.calendar.day09
 
+import me.peckb.aoc._2022.calendar.day09.Day09.Direction.*
 import javax.inject.Inject
 
 import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
@@ -10,122 +11,58 @@ class Day09 @Inject constructor(
   private val generatorFactory: InputGeneratorFactory,
 ) {
   fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::movement) { input ->
-    var headLocation = Location(0, 0)
-    var tailLocation = Location(0, 0)
-
-    val visitedLocation = mutableListOf<Location>().also { it.add(tailLocation) }
-
-    input.forEach { movement ->
-      repeat(movement.steps) {
-        when (movement.direction) {
-          Direction.LEFT -> {
-            headLocation = Location(headLocation.x - 1, headLocation.y)
-          }
-
-          Direction.RIGHT -> {
-            headLocation = Location(headLocation.x + 1, headLocation.y)
-          }
-
-          Direction.DOWN -> {
-            headLocation = Location(headLocation.x, headLocation.y + 1)
-          }
-
-          Direction.UP -> {
-            headLocation = Location(headLocation.x, headLocation.y - 1)
-          }
-        }
-        if (abs(headLocation.x - tailLocation.x) > 1 || abs(headLocation.y - tailLocation.y) > 1) {
-          tailLocation = Location(tailLocation.x + headLocation.x.compareTo(tailLocation.x), tailLocation.y + headLocation.y.compareTo(tailLocation.y))
-          visitedLocation.add(tailLocation)
-        }
-      }
-    }
-
-    visitedLocation.toSet().size
+    findUniqueTailLocations(input, children = 1)
   }
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::movement) { input ->
-    var headLocation = Location(0, 0)
-    var c1 = Location(0, 0)
-    var c2 = Location(0, 0)
-    var c3 = Location(0, 0)
-    var c4 = Location(0, 0)
-    var c5 = Location(0, 0)
-    var c6 = Location(0, 0)
-    var c7 = Location(0, 0)
-    var c8 = Location(0, 0)
-    var c9 = Location(0, 0)
+    findUniqueTailLocations(input, children = 9)
+  }
 
-    val visitedLocation = mutableListOf<Location>().also { it.add(c9) }
+  private fun findUniqueTailLocations(input: Sequence<Movement>, children: Int): Int {
+    val rope = (0..children).map { Location(0, 0) }.toMutableList()
+
+    val lastTailVisitedLocation = mutableSetOf<Location>().also { it.add(rope.last()) }
 
     input.forEach { movement ->
       repeat(movement.steps) {
-        when (movement.direction) {
-          Direction.LEFT -> {
-            headLocation = Location(headLocation.x - 1, headLocation.y)
-          }
+        val head = rope[0]
+        rope[0] = when (movement.direction) {
+          LEFT -> Location(head.x - 1, head.y)
+          RIGHT -> Location(head.x + 1, head.y)
+          DOWN -> Location(head.x, head.y + 1)
+          UP -> Location(head.x, head.y - 1)
+        }
 
-          Direction.RIGHT -> {
-            headLocation = Location(headLocation.x + 1, headLocation.y)
-          }
+        var parentIndex = 0
+        var shouldCheckChild = true
+        while (shouldCheckChild && parentIndex < rope.size - 1) {
+          val parent = rope[parentIndex]
+          val child = rope[parentIndex + 1]
 
-          Direction.DOWN -> {
-            headLocation = Location(headLocation.x, headLocation.y + 1)
+          if (abs(parent.x - child.x) > 1 || abs(parent.y - child.y) > 1) {
+            rope[parentIndex + 1] = Location(
+              x = child.x + parent.x.compareTo(child.x),
+              y = child.y + parent.y.compareTo(child.y),
+            ).also {
+              if (parentIndex + 1 == children) lastTailVisitedLocation.add(it)
+            }
+            parentIndex++
+          } else {
+            shouldCheckChild = false
           }
-
-          Direction.UP -> {
-            headLocation = Location(headLocation.x, headLocation.y - 1)
-          }
-        }
-        // c1
-        if (abs(headLocation.x - c1.x) > 1 || abs(headLocation.y - c1.y) > 1) {
-          c1 = Location(c1.x + headLocation.x.compareTo(c1.x), c1.y + headLocation.y.compareTo(c1.y))
-        }
-        // c2
-        if (abs(c1.x - c2.x) > 1 || abs(c1.y - c2.y) > 1) {
-          c2 = Location(c2.x + c1.x.compareTo(c2.x), c2.y + c1.y.compareTo(c2.y))
-        }
-        // c3
-        if (abs(c2.x - c3.x) > 1 || abs(c2.y - c3.y) > 1) {
-          c3 = Location(c3.x + c2.x.compareTo(c3.x), c3.y + c2.y.compareTo(c3.y))
-        }
-        // c4
-        if (abs(c3.x - c4.x) > 1 || abs(c3.y - c4.y) > 1) {
-          c4 = Location(c4.x + c3.x.compareTo(c4.x), c4.y + c3.y.compareTo(c4.y))
-        }
-        // c5
-        if (abs(c4.x - c5.x) > 1 || abs(c4.y - c5.y) > 1) {
-          c5 = Location(c5.x + c4.x.compareTo(c5.x), c5.y + c4.y.compareTo(c5.y))
-        }
-        // c6
-        if (abs(c5.x - c6.x) > 1 || abs(c5.y - c6.y) > 1) {
-          c6 = Location(c6.x + c5.x.compareTo(c6.x), c6.y + c5.y.compareTo(c6.y))
-        }
-        // c7
-        if (abs(c6.x - c7.x) > 1 || abs(c6.y - c7.y) > 1) {
-          c7 = Location(c7.x + c6.x.compareTo(c7.x), c7.y + c6.y.compareTo(c7.y))
-        }
-        // c8
-        if (abs(c7.x - c8.x) > 1 || abs(c7.y - c8.y) > 1) {
-          c8 = Location(c8.x + c7.x.compareTo(c8.x), c8.y + c7.y.compareTo(c8.y))
-        }
-        // c9
-        if (abs(c8.x - c9.x) > 1 || abs(c8.y - c9.y) > 1) {
-          c9 = Location(c9.x + c8.x.compareTo(c9.x), c9.y + c8.y.compareTo(c9.y))
-          visitedLocation.add(c9)
         }
       }
     }
 
-    visitedLocation.toSet().size
+    return lastTailVisitedLocation.size
   }
 
   private fun movement(line: String) = line.split(" ").let { (d, s) ->
     val direction = when (d) {
-      "R" -> Direction.RIGHT
-      "L" -> Direction.LEFT
-      "U" -> Direction.UP
-      "D" -> Direction.DOWN
+      "R" -> RIGHT
+      "L" -> LEFT
+      "U" -> UP
+      "D" -> DOWN
       else -> throw IllegalArgumentException("Invalid Direction $d")
     }
     val steps = s.toInt()
