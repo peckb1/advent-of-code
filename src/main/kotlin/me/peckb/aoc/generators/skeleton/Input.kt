37c6e730cd1e799449b57d355bb94c8edd941e38
Brian Peck <peckb@squareup.com>
2022-12-10 11:20:45 -0800

    Use private AoC repo input folder during Skeleton generation

--- src/main/kotlin/me/peckb/aoc/generators/skeleton/Input.kt
+++ src/main/kotlin/me/peckb/aoc/generators/skeleton/Input.kt
@@ -1,12 +1,12 @@
 package me.peckb.aoc.generators.skeleton
 
-import me.peckb.aoc.generators.skeleton.SkeletonGenerator.Companion.TEST_DIRECTORY
+import me.peckb.aoc.generators.skeleton.SkeletonGenerator.Companion.INPUT_DIRECTORY
 import java.nio.file.Files
 import java.nio.file.Paths
 
 object Input {
   fun generateInput(year: String, day: String) {
-    val directory = "$TEST_DIRECTORY/resources/$year"
+    val directory = "$INPUT_DIRECTORY/$year"
     val parentDirectory = Paths.get(directory)
     val path = Paths.get("$directory/day$day.input")
     if (Files.notExists(parentDirectory)) {
