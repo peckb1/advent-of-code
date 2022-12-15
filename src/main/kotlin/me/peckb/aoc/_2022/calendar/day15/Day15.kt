57ffb8abef91cb2ec977af38e634ab3919b2eaf9
Brian Peck <peckb@squareup.com>
2022-12-15 01:18:33 -0800

    some more quick cleanup

--- src/main/kotlin/me/peckb/aoc/_2022/calendar/day15/Day15.kt
+++ src/main/kotlin/me/peckb/aoc/_2022/calendar/day15/Day15.kt
@@ -39,18 +39,18 @@ class Day15 @Inject constructor(
   }
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::sensors) { input ->
-    getMissingFrequency(input.toList())
+    findFrequence(input.toList())
   }
 
   // split out into a new function for doing early return shenanigans
-  private fun getMissingFrequency(sensors: List<Sensor>): Long {
+  private fun findFrequence(sensors: List<Sensor>): Long {
     (0..4_000_000).forEach { row ->
       // this will be the current maximum x value for a given row that is covered by a sensor
       // and within the distance do its closest beacon
       var maxCoverageXIndex = 0
       // this gives us a list xValue ranges aInt the row that sensors
       // would consider within the distance from them to their closest beacon
-      val coverageRanges = rowCoverageRange(sensors, row)
+      val coverageRanges = rowCoverage(sensors, row)
         .sortedBy { it.first } // sorted to start from the front of the row first
 
       // we iterate over that range and see if we can find a "gap"
@@ -58,39 +58,39 @@ class Day15 @Inject constructor(
       // by coverage, but not covered itself.
       // This would be the single point that is surrounded by Sensors, but not close enough
       // to be the close beacon, and therefore it is our point!
-      coverageRanges.forEach { (startX, endX) ->
+      coverageRanges.forEach { (start, end) ->
         // if the start of our x value is one past (as our ranges are inclusive) the previous maximum
         // then we have just jumped passed the gap and the previous X for this row is our hidden beacon!
-        if (startX > maxCoverageXIndex + 1) return (startX - 1) * 4_000_000L + row
+        if (start > maxCoverageXIndex + 1) return (start - 1) * 4_000_000L + row
 
         // if this range isn't a gap, then ensure that our current upper X index
         // is the largest X index that we have seen so far
-        maxCoverageXIndex = max(maxCoverageXIndex, endX)
+        maxCoverageXIndex = max(maxCoverageXIndex, end)
       }
     }
     return -1
   }
 
-  private fun rowCoverageRange(sensors: List<Sensor>, yIndexOfPossibleGrid: Int): List<Pair<Int, Int>> {
+  private fun rowCoverage(sensors: List<Sensor>, row: Int): List<Pair<Int, Int>> {
     return sensors.mapNotNull { sensor ->
       // what's the distance to our closest beacon?
       val distance = sensor.closestBeaconDistance
-      // what's the distance between `y` value of our sensor, or the y index of our grid
-      val yDistanceOffset = abs(sensor.y - yIndexOfPossibleGrid)
+      // what's the distance between `y` value of our sensor, or the row value
+      val yOffset = abs(sensor.y - row)
 
       // if distance from sensor to the row we're at is larger than the closest beacon
       // then this row will not have any "coverage" inside the area of the sensor,
       // so we care about when there will be part of this row that fall within
       // the same range of the sensor to its closest beacon
-      if (yDistanceOffset > distance) {
+      if (yOffset > distance) {
         null
       } else {
         // the only part of the row that has coverage are also items that fit within the
         // sensor distance so the Sensor at x=13, y=2 on row 0 would have coverage
         // from x = 12 to x = 14
-        val startXDistanceOffset = sensor.x - (distance - yDistanceOffset)
-        val endXDistanceOffset = sensor.x + (distance - yDistanceOffset)
-        startXDistanceOffset to endXDistanceOffset
+        val startOffset = sensor.x - (distance - yOffset)
+        val endOffset = sensor.x + (distance - yOffset)
+        startOffset to endOffset
       }
     }
   }
@@ -98,18 +98,22 @@ class Day15 @Inject constructor(
   private fun sensors(line: String): Sensor {
     val (sensorDescription, beaconDescription) = line.split(":")
 
-    val (beaconX, beaconY) = beaconDescription.split(" at ")[1].split(", ").let { (xData, yData) ->
-      asInt(xData) to asInt(yData)
-    }
+    val (beaconX, beaconY) = findXYStrings(beaconDescription)
+      .let { (xData, yData) ->
+        asInt(xData) to asInt(yData)
+      }
 
-    return sensorDescription.split(" at ")[1].split(", ").let { (xData, yData) ->
-      val x = asInt(xData)
-      val y = asInt(yData)
-      Sensor(x, y, distanceBetween(x, y, beaconX, beaconY))
-    }
+    return findXYStrings(sensorDescription)
+      .let { (xData, yData) ->
+        val x = asInt(xData)
+        val y = asInt(yData)
+        Sensor(x, y, distanceBetween(x, y, beaconX, beaconY))
+      }
   }
 
   private fun asInt(input: String) = input.split("=")[1].toInt()
+
+  private fun findXYStrings(input: String) = input.split(" at ")[1].split(", ")
 
   private fun distanceBetween(x1: Int, y1: Int, x2: Int, y2: Int) = abs(x1 - x2) + abs(y1 - y2)
 
