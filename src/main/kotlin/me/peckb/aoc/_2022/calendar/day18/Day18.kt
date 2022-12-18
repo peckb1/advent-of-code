211b026359f8941599cc43879f413d1d064430ad
Brian Peck <peckb@squareup.com>
2022-12-17 23:39:59 -0800

    cleanup land

--- src/main/kotlin/me/peckb/aoc/_2022/calendar/day18/Day18.kt
+++ src/main/kotlin/me/peckb/aoc/_2022/calendar/day18/Day18.kt
@@ -10,70 +10,41 @@ class Day18 @Inject constructor(
 ) {
   fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::cubeLocation) { input ->
     val cubes = hashSetOf<CubeLocation>()
-    input.forEach { cube ->
-      cubes.add(cube)
-    }
-
-    cubes.sumOf { cube ->
-      val (x, y, z) = cube
-      val front = CubeLocation(x, y, z + 1)
-      val back = CubeLocation(x, y, z - 1)
-      val up = CubeLocation(x, y + 1, z)
-      val down = CubeLocation(x, y - 1, z)
-      val right = CubeLocation(x + 1, y, z)
-      val left = CubeLocation(x - 1, y, z)
-
-      listOf(
-        cubes.contains(front), cubes.contains(back),
-        cubes.contains(up), cubes.contains(down),
-        cubes.contains(right), cubes.contains(left)
-      ).count { !it }
-    }
+    input.forEach { cube -> cubes.add(cube) }
+    cubes.sumOf { cube -> cube.neighborCubes().count { !cubes.contains(it) } }
   }
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::cubeLocation) { input ->
     val cubes = hashSetOf<CubeLocation>()
-    input.forEach { cube ->
-      cubes.add(cube.withCubes(cubes))
-    }
 
-    val cubeSurfaceArea = cubes.sumOf { cube ->
-      val (x, y, z) = cube
-      val front = CubeLocation(x, y, z + 1).withCubes(cube.cubes)
-      val back = CubeLocation(x, y, z - 1).withCubes(cube.cubes)
-      val up = CubeLocation(x, y + 1, z).withCubes(cube.cubes)
-      val down = CubeLocation(x, y - 1, z).withCubes(cube.cubes)
-      val right = CubeLocation(x + 1, y, z).withCubes(cube.cubes)
-      val left = CubeLocation(x - 1, y, z).withCubes(cube.cubes)
-      listOf(
-        cubes.contains(front), cubes.contains(back),
-        cubes.contains(up), cubes.contains(down),
-        cubes.contains(right), cubes.contains(left)
-      ).count { !it }
+    var min = Int.MAX_VALUE
+    var max = Int.MIN_VALUE
+    input.forEach { cube ->
+      cubes.add(cube)
+      min = minOf(min, cube.x, cube.y, cube.z)
+      max = maxOf(max, cube.x, cube.y, cube.z)
     }
+    // add buffer
+    min--
+    max++
+
+    val cubeSurfaceArea = cubes.sumOf { cube -> cube.neighborCubes().count { !cubes.contains(it) } }
 
     val pocketCubes = mutableSetOf<CubeLocation>()
     val outerAir = mutableSetOf<CubeLocation>()
+    val solver = CubeDijkstra()
 
-    cubes.forEachIndexed { n, cube ->
-      val (x, y, z) = cube
-      val front = CubeLocation(x, y, z + 1).withCubes(cube.cubes)
-      val back = CubeLocation(x, y, z - 1).withCubes(cube.cubes)
-      val up = CubeLocation(x, y + 1, z).withCubes(cube.cubes)
-      val down = CubeLocation(x, y - 1, z).withCubes(cube.cubes)
-      val right = CubeLocation(x + 1, y, z).withCubes(cube.cubes)
-      val left = CubeLocation(x - 1, y, z).withCubes(cube.cubes)
-
-      val solver = CubeDijkstra()
-      listOf(front, back, up, down, right, left)
+    cubes.forEach { cube ->
+      cube.neighborCubes()
+        .map { it.withCubes(cubes).withBounds(min, max) }
         .filter { !cubes.contains(it) }
         .filter { !outerAir.contains(it) }
         .filter { !pocketCubes.contains(it) }
-        .forEach { airCube ->
-          val airNeighbors = solver.solve(airCube).keys
-          val pocketCube = !airNeighbors.any { it.x == -1 || it.y == -1 || it.z == -1 } &&
-            !airNeighbors.any { it.x == 22 || it.y == 22 || it.z == 22 }
-          if (pocketCube) {
+        .forEach { unseenAirCube ->
+          val airNeighbors = solver.solve(unseenAirCube).keys
+          val isInAPocket = !airNeighbors.any { it.x == min || it.y == min || it.z == min } &&
+            !airNeighbors.any { it.x == max || it.y == max || it.z == max }
+          if (isInAPocket) {
             pocketCubes.addAll(airNeighbors)
           } else {
             outerAir.addAll(airNeighbors)
@@ -81,8 +52,37 @@ class Day18 @Inject constructor(
         }
     }
 
-    cubeSurfaceArea - pocketCubes.sumOf { airCube ->
-      val (x, y, z) = airCube
+    val airBubbleSurfaceArea = pocketCubes.sumOf { airCube ->
+      airCube.neighborCubes().count { cubes.contains(it) }
+    }
+
+    cubeSurfaceArea - airBubbleSurfaceArea
+  }
+
+  private fun cubeLocation(line: String) : CubeLocation {
+    return line.split(",").map { it.toInt() }.let { (x, y, z) -> CubeLocation(x, y, z) }
+  }
+
+  data class CubeLocation(val x: Int, val y: Int, val z: Int) : GenericIntDijkstra.DijkstraNode<CubeLocation> {
+    private var min: Int = Int.MAX_VALUE
+    private var max: Int = Int.MIN_VALUE
+    private lateinit var cubes: HashSet<CubeLocation>
+
+    fun withCubes(cubes: HashSet<CubeLocation>) = apply { this.cubes = cubes }
+
+    fun withBounds(min: Int, max: Int) = apply {
+      this.min = min
+      this.max = max
+    }
+
+    override fun neighbors(): Map<CubeLocation, Int> {
+      return neighborCubes()
+        .map { it.withCubes(cubes).withBounds(min, max) }
+        .filter { it.x in (min..max) && it.y in (min..max) && it.z in (min..max) }
+        .filter { !cubes.contains(it) }.associateWith { 1 }
+    }
+
+    fun neighborCubes(): List<CubeLocation> {
       val front = CubeLocation(x, y, z + 1)
       val back = CubeLocation(x, y, z - 1)
       val up = CubeLocation(x, y + 1, z)
@@ -90,34 +90,7 @@ class Day18 @Inject constructor(
       val right = CubeLocation(x + 1, y, z)
       val left = CubeLocation(x - 1, y, z)
 
-      listOf(front, back, up, down, right, left).count {
-        cubes.contains(it)
-      }
-    }
-  }
-
-
-  private fun cubeLocation(line: String) : CubeLocation {
-    return line.split(",").map { it.toInt() }.let { (x, y, z) -> CubeLocation(x, y, z) }
-  }
-
-  data class CubeLocation(val x: Int, val y: Int, val z: Int) : GenericIntDijkstra.DijkstraNode<CubeLocation> {
-    var cubes: HashSet<CubeLocation> = hashSetOf()
-
-    fun withCubes(cubes: HashSet<CubeLocation>) = apply { this.cubes = cubes }
-
-    override fun neighbors(): Map<CubeLocation, Int> {
-      val front = CubeLocation(x, y, z + 1).withCubes(cubes)
-      val back = CubeLocation(x, y, z - 1).withCubes(cubes)
-      val up = CubeLocation(x, y + 1, z).withCubes(cubes)
-      val down = CubeLocation(x, y - 1, z).withCubes(cubes)
-      val right = CubeLocation(x + 1, y, z).withCubes(cubes)
-      val left = CubeLocation(x - 1, y, z).withCubes(cubes)
-
       return listOf(front, back, up, down, right, left)
-        // TODO: remove hard coded
-        .filter { it.x in (-1..22) && it.y in (-1..22) && it.z in (-1..22) }
-        .filter { !cubes.contains(it) }.associateWith { 1 }
     }
   }
 
