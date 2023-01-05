bfd2b3313edc37608244cbe0669095037604a56f
Brian Peck <peckb@squareup.com>
2023-01-05 15:16:32 -0800

    Day 01

---
+++ src/test/kotlin/me/peckb/aoc/_2020/TestDayComponent.kt
@@ -0,0 +1,14 @@
+package me.peckb.aoc._2020
+
+import dagger.Component
+import javax.inject.Singleton
+
+import me.peckb.aoc.DayComponent
+import me.peckb.aoc.InputModule
+import me.peckb.aoc._2020.calendar.day01.Day01Test
+
+@Singleton
+@Component(modules = [InputModule::class])
+internal interface TestDayComponent : DayComponent {
+  fun inject(day01Test: Day01Test)
+}
