2b5213d3164332975c462c85d9328f4639c75ce1
Brian Peck <peckb@squareup.com>
2023-07-19 21:14:45 -0700

    Refactor out the program running

--- src/main/kotlin/me/peckb/aoc/_2020/calendar/day08/Day08.kt
+++ src/main/kotlin/me/peckb/aoc/_2020/calendar/day08/Day08.kt
@@ -8,6 +8,76 @@ import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
 class Day08 @Inject constructor(
   private val generatorFactory: InputGeneratorFactory,
 ) {
+  fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::instruction) { input ->
+    val instructions = input.toList()
+
+    runProgram(instructions.size) { instructions[it] }.accumulator
+  }
+
+  fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::instruction) { input ->
+    val instructions = input.toList()
+    val jumpIndices = instructions.indices.filter { instructions[it].operation == JUMP }
+    val noOpOverride = Instruction(NO_OP, 0)
+
+    var terminated = false
+    var accumulator = 0
+    var testJumpIndicesIndex = 0
+
+    while(!terminated) {
+      val (acc, finished) = runProgram(instructions.size) { index ->
+        if (index == jumpIndices[testJumpIndicesIndex]) {
+          noOpOverride
+        } else {
+          instructions[index]
+        }
+      }
+      if (finished) {
+        accumulator = acc
+        terminated = true
+      } else {
+        testJumpIndicesIndex += 1
+      }
+    }
+
+    accumulator
+  }
+
+  private fun instruction(line: String): Instruction = line.split(" ")
+    .let { (operation, argument) ->
+      Instruction(
+        Operation.fromKeyword(operation),
+        argument.toInt()
+      )
+    }
+
+  private fun runProgram(maxInstructions: Int, getInstruction: (Int) -> Instruction): ProgramResult {
+    var index = 0
+    var duplicateInstruction = -1
+    var accumulator = 0
+
+    val instructionsRan = mutableSetOf<Int>()
+
+    while(index < maxInstructions) {
+      instructionsRan.add(index)
+      val instruction = getInstruction(index)
+
+      when (instruction.operation) {
+        ACCUMULATOR -> {
+          accumulator += instruction.argument
+          index++
+        }
+        JUMP -> index += instruction.argument
+        NO_OP -> index++
+      }
+      if (instructionsRan.contains(index)) {
+        duplicateInstruction = index
+        index = maxInstructions
+      }
+    }
+
+    return ProgramResult(accumulator, duplicateInstruction == -1)
+  }
+
   enum class Operation(val keyword: String) {
     ACCUMULATOR("acc"),
     JUMP("jmp"),
@@ -25,86 +95,5 @@ class Day08 @Inject constructor(
     val argument: Int
   )
 
-  fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::instruction) { input ->
-    val instructions = input.toList()
-
-    var accumulator = 0L
-    var index = 0
-    val instructionsRan= mutableSetOf<Int>()
-
-    while(index < instructions.size) {
-      instructionsRan.add(index)
-      val instruction = instructions[index]
-      when (instruction.operation) {
-        ACCUMULATOR -> {
-          accumulator += instruction.argument
-          index++
-        }
-        JUMP -> index += instruction.argument
-        NO_OP -> index++
-      }
-      if (instructionsRan.contains(index)) {
-        index = instructions.size
-      }
-    }
-
-    accumulator
-  }
-
-  fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::instruction) { input ->
-    val instructions = input.toList()
-    val jumpIndices = instructions.indices.filter { instructions[it].operation == JUMP }
-    val noOpOverride = Instruction(NO_OP, 0)
-
-    var terminated = false
-    var accumulator = 0L
-    var testJumpIndicesIndex = 0
-
-    while(!terminated) {
-
-      var index = 0
-      var duplicateInstruction = -1
-
-      val instructionsRan= mutableSetOf<Int>()
-
-      while(index < instructions.size) {
-        instructionsRan.add(index)
-        val instruction = if (index == jumpIndices[testJumpIndicesIndex]) {
-          noOpOverride
-        } else {
-          instructions[index]
-        }
-
-        when (instruction.operation) {
-          ACCUMULATOR -> {
-            accumulator += instruction.argument
-            index++
-          }
-          JUMP -> index += instruction.argument
-          NO_OP -> index++
-        }
-        if (instructionsRan.contains(index)) {
-          duplicateInstruction = index
-          index = instructions.size
-        }
-      }
-
-      if (duplicateInstruction == -1) {
-        terminated = true
-      } else {
-        accumulator = 0
-        testJumpIndicesIndex += 1
-      }
-    }
-
-    accumulator
-  }
-
-  private fun instruction(line: String): Instruction = line.split(" ")
-    .let { (operation, argument) ->
-      Instruction(
-        Operation.fromKeyword(operation),
-        argument.toInt()
-      )
-    }
+  data class ProgramResult(val accumulator: Int, val terminated: Boolean)
 }
