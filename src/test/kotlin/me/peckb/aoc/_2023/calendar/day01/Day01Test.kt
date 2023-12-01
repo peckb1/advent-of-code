b1aca9f50bf4dbe63023d4f4f218113f7632cae3
Brian Peck <Catbert321@gmail.com>
2023-11-30 21:45:22 -0800

    Day 01 2023 (#227)

---
+++ src/test/kotlin/me/peckb/aoc/_2023/calendar/day01/Day01Test.kt
@@ -0,0 +1,33 @@
+package me.peckb.aoc._2023.calendar.day01
+
+import javax.inject.Inject
+
+
+import me.peckb.aoc._2023.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Day01Test {
+  @Inject
+  lateinit var day01: Day01
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Test
+  fun testDay01PartOne() {
+    assertEquals(54667, day01.partOne(DAY_01))
+  }
+
+  @Test
+  fun testDay01PartTwo() {
+    assertEquals(54203, day01.partTwo(DAY_01))
+  }
+
+  companion object {
+    private const val DAY_01: String = "advent-of-code-input/2023/day01.input"
+  }
+}
