b516c7043507b4b2ee5a08d73ad3d25e7486aff5
Brian Peck <Catbert321@gmail.com>
2023-12-01 21:32:30 -0800

    Day 02 2025 (#228)

--- src/test/kotlin/me/peckb/aoc/_2023/TestDayComponent.kt
+++ src/test/kotlin/me/peckb/aoc/_2023/TestDayComponent.kt
@@ -1,6 +1,7 @@
 package me.peckb.aoc._2023
 
 import dagger.Component
+import me.peckb.aoc._2023.calendar.day02.Day02Test
 import javax.inject.Singleton
 
 import me.peckb.aoc.DayComponent
@@ -11,4 +12,5 @@ import me.peckb.aoc._2023.calendar.day01.Day01Test
 @Component(modules = [InputModule::class])
 internal interface TestDayComponent : DayComponent {
   fun inject(day01Test: Day01Test)
+  fun inject(day02Test: Day02Test)
 }
