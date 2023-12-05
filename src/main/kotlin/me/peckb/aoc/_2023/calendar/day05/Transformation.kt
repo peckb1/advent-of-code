95734d662a55e1eac2492da838dd8977aa28fb8b
Brian Peck <peckb@squareup.com>
2023-12-04 22:56:34 -0800

    exraneous storage

--- src/main/kotlin/me/peckb/aoc/_2023/calendar/day05/Transformation.kt
+++ src/main/kotlin/me/peckb/aoc/_2023/calendar/day05/Transformation.kt
@@ -1,6 +1,6 @@
 package me.peckb.aoc._2023.calendar.day05
 
-class Transformation(private val source: Long, private val destination: Long, private val range: Long) {
+class Transformation(private val source: Long, private val destination: Long, range: Long) {
   private val transformationRange: LongRange = (source until source + range)
   private val reverseTransformationRange: LongRange = (destination until destination + range)
 
