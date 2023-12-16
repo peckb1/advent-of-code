0c6650dbf3e18c9b4ee021c1b4ebb6a66d0c3730
Brian Peck <peckb@squareup.com>
2023-12-15 22:54:59 -0800

    cleaner solution

--- src/main/kotlin/me/peckb/aoc/_2023/calendar/day16/Day16.kt
+++ src/main/kotlin/me/peckb/aoc/_2023/calendar/day16/Day16.kt
@@ -1,6 +1,6 @@
 package me.peckb.aoc._2023.calendar.day16
 
-import me.peckb.aoc._2023.calendar.day16.Day16.Direction.*
+import me.peckb.aoc._2023.calendar.day16.Direction.*
 import javax.inject.Inject
 
 import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
@@ -48,207 +48,28 @@ class Day16 @Inject constructor(
     }.maxByOrNull { it }
   }
 
-  fun fireLaser(caveSystem: List<List<CaveArea>>, laserStart : Pair<Location, Direction>) {
+  private fun fireLaser(caveSystem: List<List<CaveArea>>, laserStart : Pair<Location, Direction>) {
     val lasers: Queue<Pair<Location, Direction>> = LinkedList()
     lasers.add(laserStart)
 
     while(lasers.isNotEmpty()) {
       val laserData = lasers.remove()
-
-      var direction = laserData.second
       val location = laserData.first
 
+      var direction = laserData.second
       var laserContinuing = true
+
       while (laserContinuing) {
         location.travel(direction)
 
         if (caveSystem.indices.contains(location.row) && caveSystem[location.row].indices.contains(location.col)) {
           val area = caveSystem[location.row][location.col]
+          val (newDirections, laserStopped, newDirection) = area.enterGoing(direction)
 
-          when (area.symbol) {
-            '|' -> {
-              when (direction) {
-                NORTH -> {
-                  if (area.southEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.southEntranceEnergized = true
-                  }
-                }
-                SOUTH -> {
-                  if (area.northEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.northEntranceEnergized = true
-                  }
-                }
-                EAST -> {
-                  if (area.westEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.westEntranceEnergized = true
-                    lasers.add(location.copy() to NORTH)
-                    lasers.add(location.copy() to SOUTH)
-                    laserContinuing = false
-                  }
-                }
-                WEST -> {
-                  if (area.eastEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.eastEntranceEnergized = true
-                    lasers.add(location.copy() to NORTH)
-                    lasers.add(location.copy() to SOUTH)
-                    laserContinuing = false
-                  }
-                }
-              }
-            }
+          direction = newDirection
+          laserContinuing = !laserStopped
+          newDirections.forEach { lasers.add(location.copy() to it) }
 
-            '-' -> {
-              when (direction) {
-                NORTH -> {
-                  if (area.southEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.southEntranceEnergized = true
-                    lasers.add(location.copy() to EAST)
-                    lasers.add(location.copy() to WEST)
-                    laserContinuing = false
-                  }
-                }
-                SOUTH -> {
-                  if (area.northEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.northEntranceEnergized = true
-                    lasers.add(location.copy() to EAST)
-                    lasers.add(location.copy() to WEST)
-                    laserContinuing = false
-                  }
-                }
-                EAST  -> {
-                  if (area.westEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.westEntranceEnergized = true
-                  }
-                }
-                WEST  -> {
-                  if (area.eastEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.eastEntranceEnergized = true
-                  }
-                }
-              }
-            }
-            '\\' -> {
-              when (direction) {
-                NORTH -> {
-                  if (area.southEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.southEntranceEnergized = true
-                    direction = WEST
-                  }
-                }
-                SOUTH -> {
-                  if (area.northEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.northEntranceEnergized = true
-                    direction = EAST
-                  }
-                }
-                EAST  -> {
-                  if (area.westEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.westEntranceEnergized = true
-                    direction = SOUTH
-                  }
-                }
-                WEST  -> {
-                  if (area.eastEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.eastEntranceEnergized = true
-                    direction = NORTH
-                  }
-                }
-              }
-            }
-            '/' -> {
-              when (direction) {
-                NORTH -> {
-                  if (area.southEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.southEntranceEnergized = true
-                    direction = EAST
-                  }
-                }
-                SOUTH -> {
-                  if (area.northEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.northEntranceEnergized = true
-                    direction = WEST
-                  }
-                }
-                EAST  -> {
-                  if (area.westEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.westEntranceEnergized = true
-                    direction = NORTH
-                  }
-                }
-                WEST  -> {
-                  if (area.eastEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.eastEntranceEnergized = true
-                    direction = SOUTH
-                  }
-                }
-              }
-            }
-            '.' -> {
-              when (direction) {
-                NORTH -> {
-                  if (area.southEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.southEntranceEnergized = true
-                  }
-                }
-                SOUTH -> {
-                  if (area.northEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.northEntranceEnergized = true
-                  }
-                }
-                EAST  -> {
-                  if (area.westEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.westEntranceEnergized = true
-                  }
-                }
-                WEST  -> {
-                  if (area.eastEntranceEnergized) {
-                    laserContinuing = false
-                  } else {
-                    area.eastEntranceEnergized = true
-                  }
-                }
-              }
-            }
-            else -> throw IllegalStateException("Unknown CaveArea [$area]")
-          }
         } else {
           laserContinuing = false
         }
@@ -266,48 +87,6 @@ class Day16 @Inject constructor(
     return caveSystem.sumOf { row ->
       row.sumOf { caveArea ->
         if (caveArea.isEnergized()) 1L else 0L
-      }
-    }
-  }
-
-  data class CaveArea(
-    val symbol: Char,
-    var eastEntranceEnergized: Boolean = false,
-    var westEntranceEnergized: Boolean = false,
-    var northEntranceEnergized: Boolean = false,
-    var southEntranceEnergized: Boolean = false,
-  ) {
-    fun isEnergized(): Boolean {
-      return eastEntranceEnergized || westEntranceEnergized || northEntranceEnergized || southEntranceEnergized
-    }
-
-    fun reset() {
-      eastEntranceEnergized = false
-      westEntranceEnergized = false
-      northEntranceEnergized = false
-      southEntranceEnergized = false
-    }
-
-    override fun toString(): String {
-      return if (isEnergized()) {
-        "#"
-      } else {
-        "."
-      }
-    }
-  }
-
-  enum class Direction {
-    NORTH, SOUTH, EAST, WEST
-  }
-
-  data class Location(var row: Int, var col: Int) {
-    fun travel(direction: Direction) {
-      when (direction) {
-        NORTH -> row--
-        SOUTH -> row++
-        EAST  -> col++
-        WEST  -> col--
       }
     }
   }
---
+++ src/main/kotlin/me/peckb/aoc/_2023/calendar/day16/Direction.kt
@@ -0,0 +1,5 @@
+package me.peckb.aoc._2023.calendar.day16
+
+enum class Direction {
+  NORTH, SOUTH, EAST, WEST, STOP
+}
