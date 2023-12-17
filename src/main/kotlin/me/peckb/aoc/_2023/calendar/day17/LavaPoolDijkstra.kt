362426bdf7e3fb967180582c7ac6acd853024a39
Brian Peck <peckb@squareup.com>
2023-12-17 12:06:19 -0800

    rename

--- src/main/kotlin/me/peckb/aoc/_2023/calendar/day17/LavaPoolDijkstra.kt
+++ src/main/kotlin/me/peckb/aoc/_2023/calendar/day17/LavaPoolDijkstra.kt
@@ -1,23 +1,23 @@
 package me.peckb.aoc._2023.calendar.day17
 
+import me.peckb.aoc._2023.calendar.day17.LavaPoolDijkstra.HeatNode
 import me.peckb.aoc.pathing.Dijkstra
 import me.peckb.aoc.pathing.DijkstraNodeWithCost
 
 class LavaPoolDijkstra(
   val lava: List<List<Int>>,
   val neighborsGenerator: (List<List<Int>>, Node) -> List<Pair<Node, Int>>
-) : Dijkstra<Node, Int, LavaPoolDijkstra.NodeWithCost> {
+) : Dijkstra<Node, Int, HeatNode> {
   override fun Int.plus(cost: Int) = this + cost
 
-  override fun Node.withCost(cost: Int) = NodeWithCost(this, cost)
+  override fun Node.withCost(cost: Int) = HeatNode(this, cost)
 
   override fun minCost(): Int = 0
 
   override fun maxCost(): Int = Int.MAX_VALUE
 
-  inner class NodeWithCost(val node: Node, private val heatLoss: Int) : DijkstraNodeWithCost<Node, Int> {
-    override fun neighbors() = neighborsGenerator(lava, node)
-      .map { NodeWithCost(it.first, it.second) }
+  inner class HeatNode(val node: Node, private val heatLoss: Int) : DijkstraNodeWithCost<Node, Int> {
+    override fun neighbors() = neighborsGenerator(lava, node).map { HeatNode(it.first, it.second) }
 
     override fun node(): Node = node
 
