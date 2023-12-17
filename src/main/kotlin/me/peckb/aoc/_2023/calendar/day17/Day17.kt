a86967d693aeba9f5ab5175f69f103ccdef7884d
Brian Peck <peckb@squareup.com>
2023-12-17 15:12:17 -0800

    Add in min four steps before exiting requirement

--- src/main/kotlin/me/peckb/aoc/_2023/calendar/day17/Day17.kt
+++ src/main/kotlin/me/peckb/aoc/_2023/calendar/day17/Day17.kt
@@ -3,12 +3,13 @@ package me.peckb.aoc._2023.calendar.day17
 import javax.inject.Inject
 
 import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
+import kotlin.math.max
 
 class Day17 @Inject constructor(
   private val generatorFactory: InputGeneratorFactory,
 ) {
   fun partOne(filename: String) = generatorFactory.forFile(filename).read { input ->
-    runDijkstra(input) { node ->
+    runDijkstra(input, minSteps = 1) { node ->
       mutableListOf<Direction>().apply {
         addAll(node.directionTraveling.turnDirections())
         if (node.stepsInDirection < 3) { add(node.directionTraveling) }
@@ -17,7 +18,7 @@ class Day17 @Inject constructor(
   }
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).read { input ->
-    runDijkstra(input) { node ->
+    runDijkstra(input, minSteps = 4) { node ->
       mutableListOf<Direction>().apply {
         if (node.stepsInDirection >= 4) { addAll(node.directionTraveling.turnDirections()) }
         if (node.stepsInDirection < 10) { add(node.directionTraveling) }
@@ -25,7 +26,7 @@ class Day17 @Inject constructor(
     }
   }
 
-  private fun runDijkstra(input: Sequence<String>, neighbors: (Node) -> List<Direction>): Int {
+  private fun runDijkstra(input: Sequence<String>, minSteps: Int, neighbors: (Node) -> List<Direction>): Int {
     val lavaPool = mutableListOf<MutableList<Int>>().apply {
       input.forEach { row -> add(row.map { it.digitToInt() }.toMutableList()) }
     }
@@ -41,13 +42,19 @@ class Day17 @Inject constructor(
       }
     }
 
-    val paths = dijkstra.solve(start, end) { n1, n2 ->
-      when (val colCompare = n1.col.compareTo(n2.col)) {
-        0 -> n1.row.compareTo(n2.row)
+    val paths = dijkstra.solve(start, end) { searchNode, goalNode ->
+      when (val colCompare = searchNode.col.compareTo(goalNode.col)) {
+        0 -> when (val rowCompare = searchNode.row.compareTo(goalNode.row)) {
+          0 -> max(minSteps - searchNode.stepsInDirection, 0)
+          else -> rowCompare
+        }
         else -> colCompare
       }
     }
 
-    return paths.filter { it.key.row == end.row && it.key.col == end.col }.minOf { it.value }
+    return paths
+      .filter { it.key.row == end.row && it.key.col == end.col }
+      .filter { it.key.stepsInDirection >= minSteps }
+      .minOf { it.value }
   }
 }
