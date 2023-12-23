4b46d768e282854380ed812a539c553de8cc3123
Brian Peck <peckb@squareup.com>
2023-12-22 19:25:54 -0800

    Ignore deletable bricks for checking for extra falls

--- src/main/kotlin/me/peckb/aoc/_2023/calendar/day22/Day22.kt
+++ src/main/kotlin/me/peckb/aoc/_2023/calendar/day22/Day22.kt
@@ -8,10 +8,56 @@ class Day22 @Inject constructor(
   private val generatorFactory: InputGeneratorFactory,
 ) {
   fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::brick) { input ->
-    val bricks = input.sortedBy { it.start.z }.toList()
+    val bricks = input.sortedBy { it.start.z }.toList().apply {
+      forEachIndexed { index, brick -> brick.fall(index, this) }
+    }
 
-    bricks.forEachIndexed { index, brick -> brick.fall(index, bricks) }
+    findDeletableBricks(bricks).size
+  }
 
+  fun partTwo(filename: String) = generatorFactory.forFile(filename).read { input ->
+    val brickInput = input.toList()
+    val ogBricks = brickInput.map(::brick)
+      .sortedBy { it.start.z } // sort the items for falling
+      .apply { forEachIndexed { index, brick -> brick.fall(index, this) } }
+      .sortedBy { it.start.z } // re-sort after falling for disintegration + falling
+
+    val deletableBricks = findDeletableBricks(ogBricks)
+
+    val times1 = mutableListOf<Long>()
+    val times2 = mutableListOf<Long>()
+    val times3 = mutableListOf<Long>()
+
+    brickInput.indices.asSequence().filterNot { deletableBricks.containsKey(ogBricks[it]) }.sumOf { indexOfBrickToRemove ->
+      val s1 = System.currentTimeMillis()
+      val bricksToMessWith = ogBricks.map { it.clone() }
+      val e1 = System.currentTimeMillis()
+
+      val s2 = System.currentTimeMillis()
+      val brickToDisintegrate = bricksToMessWith[indexOfBrickToRemove]
+      val bricksAfterDisintegration = bricksToMessWith.minus(brickToDisintegrate)
+      val e2 = System.currentTimeMillis()
+
+      val s3 = System.currentTimeMillis()
+      bricksAfterDisintegration.withIndex().count { (index, brick) ->
+        brick.fall(index, bricksAfterDisintegration)
+      }.also {
+        val e3 = System.currentTimeMillis()
+        times1.add(e1 - s1)
+        times2.add(e2 - s2)
+        times3.add(e3 - s3)
+      }
+    }.also {
+      println(times1.sum())
+      println(times1)
+      println(times2.sum())
+      println(times2)
+      println(times3.sum())
+      println(times3)
+    }
+  }
+
+  private fun findDeletableBricks(bricks: List<Brick>): Map<Brick, List<Brick>> {
     val brickByZHeight = bricks.groupBy { it.start.z }.withDefault { emptyList() }
     val brickToSupportsMap: MutableMap<Brick, List<Brick>> = mutableMapOf()
     val brickToReliantMap: MutableMap<Brick, List<Brick>> = mutableMapOf()
@@ -34,27 +80,8 @@ class Day22 @Inject constructor(
       }
     }
 
-    brickToReliantMap.count { (me, bricksThatRelyOnMe) ->
+    return brickToReliantMap.filter { (me, bricksThatRelyOnMe) ->
       me.canBeDeleted(bricksThatRelyOnMe, brickToSupportsMap)
-    }
-  }
-
-  fun partTwo(filename: String) = generatorFactory.forFile(filename).read { input ->
-    val brickInput = input.toList()
-    val ogBricks = brickInput.map(::brick)
-      .sortedBy { it.start.z } // sort the items for falling
-      .apply { forEachIndexed { index, brick -> brick.fall(index, this) } }
-      .sortedBy { it.start.z } // re-sort after falling for disintegration + falling
-
-    brickInput.indices.sumOf { indexOfBrickToRemove ->
-      val bricksToMessWith = ogBricks.map { it.clone() }
-
-      val brickToDisintegrate = bricksToMessWith[indexOfBrickToRemove]
-      val bricksAfterDisintegration = bricksToMessWith.minus(brickToDisintegrate)
-
-      bricksAfterDisintegration.withIndex().count { (index, brick) ->
-        brick.fall(index, bricksAfterDisintegration)
-      }
     }
   }
 
