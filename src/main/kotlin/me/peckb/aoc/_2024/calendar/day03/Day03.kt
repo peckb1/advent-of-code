9c21ac7c68e76e143c2e9cab574e7e222563b7f2
Brian Peck <peckb@squareup.com>
2024-12-02 22:05:57 -0800

    Utilize Regez groups 'for fun'

--- src/main/kotlin/me/peckb/aoc/_2024/calendar/day03/Day03.kt
+++ src/main/kotlin/me/peckb/aoc/_2024/calendar/day03/Day03.kt
@@ -8,9 +8,9 @@ class Day03 @Inject constructor(
 ) {
   fun partOne(filename: String) = generatorFactory.forFile(filename).read { lines ->
     lines.sumOf { line ->
-      val matches = "mul\\(\\d+,\\d+\\)".toRegex().findAll(line)
+      val matches = "mul\\((?<d1>\\d+),(?<d2>\\d+)\\)".toRegex().findAll(line)
 
-      matches.sumOf { match -> match.value.getMultiplicationResult() }
+      matches.sumOf { it.getMultiplicationResult() }
     }
   }
 
@@ -18,7 +18,7 @@ class Day03 @Inject constructor(
     var enabled = true
 
     lines.sumOf { line ->
-      val mulMatches = "mul\\(\\d+,\\d+\\)".toRegex().findAll(line)
+      val mulMatches = "mul\\((?<d1>\\d+),(?<d2>\\d+)\\)".toRegex().findAll(line)
       val doMatches = "do\\(\\)".toRegex().findAll(line)
       val dontMatches = "don\'t\\(\\)".toRegex().findAll(line)
 
@@ -27,12 +27,10 @@ class Day03 @Inject constructor(
       var count = 0L
 
       sortedMatches.forEach { match ->
-        val value = match.value
-
-        when (value.take(3)) {
+        when (match.value.take(3)) {
           "do(" -> enabled = true
           "don" -> enabled = false
-          "mul" -> if (enabled) { count += value.getMultiplicationResult() }
+          "mul" -> if (enabled) { count += match.getMultiplicationResult() }
         }
       }
 
@@ -40,6 +38,5 @@ class Day03 @Inject constructor(
     }
   }
 
-  private fun String.getMultiplicationResult() =
-    this.drop(4).dropLast(1).split(",").map { it.toInt() }.reduce(Int::times)
+  private fun MatchResult.getMultiplicationResult() = groupValues[1].toInt() * groupValues[2].toInt()
 }
