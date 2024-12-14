118883cab224f7a6850f6f5ecf1d18fe2cd06d52
Brian Peck <Catbert321@gmail.com>
2024-12-13 22:33:55 -0800

    Day 14 2024 (#274)

---
+++ src/main/kotlin/me/peckb/aoc/_2024/calendar/day14/README.md
@@ -0,0 +1,1 @@
+## [Day 14: Restroom Redoubt](https://adventofcode.com/2024/day/14)
---
+++ src/test/kotlin/me/peckb/aoc/_2024/calendar/day14/Day14Test.kt
@@ -0,0 +1,32 @@
+package me.peckb.aoc._2024.calendar.day14
+
+import javax.inject.Inject
+
+import me.peckb.aoc._2024.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Day14Test {
+  @Inject
+  lateinit var day14: Day14
+
+  @BeforeEach
+  fun setup() {
+    DaggerTestDayComponent.create().inject(this)
+  }
+
+  @Test
+  fun testDay14PartOne() {
+    assertEquals(229980828, day14.partOne(DAY_14))
+  }
+
+  @Test
+  fun testDay14PartTwo() {
+    assertEquals(7132, day14.partTwo(DAY_14))
+  }
+
+  companion object {
+    private const val DAY_14: String = "advent-of-code-input/2024/day14.input"
+  }
+}
