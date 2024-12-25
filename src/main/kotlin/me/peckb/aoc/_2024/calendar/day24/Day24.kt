53e42e2654d343b13f9a658653486559ca4a268f
Brian Peck <peckb@squareup.com>
2024-12-24 16:29:35 -0800

    Cleanup

--- src/main/kotlin/me/peckb/aoc/_2024/calendar/day24/Day24.kt
+++ src/main/kotlin/me/peckb/aoc/_2024/calendar/day24/Day24.kt
@@ -19,28 +19,21 @@ class Day24 @Inject constructor(
         return@input
       }
       if (readingBase) {
-        line.split(": ").let { (key, value) ->
-          data[key] = lazy { value.toInt() }
-        }
+        line.split(": ").let { (key, value) -> data[key] = lazy { value.toInt() } }
       } else {
-        line.split(" ").let { (first, operator, second, _, result) ->
-          if (result.startsWith('z')) {
-            zValues.add(result)
-          }
-          val op = when (operator) {
-            "AND" -> Int::and
-            "OR" -> Int::or
-            "XOR" -> Int::xor
-            else -> throw IllegalArgumentException("Unknown operation $operator")
-          }
+        line.split(" ").let { (first, operations, second, _, result) ->
+          if (result.startsWith('z')) { zValues.add(result) }
+
+          val op = findOperation(operations)
           data[result] = lazy { op(data[first]!!.value, data[second]!!.value) }
         }
       }
     }
 
-    val zResults = zValues.sorted().map { data[it]!!.value }.reversed()
-
-    zResults.joinToString("").toLong(2)
+    zValues.sortedDescending()
+      .map { data[it]!!.value }
+      .joinToString("")
+      .toLong(2)
   }
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).read { input ->
@@ -50,17 +43,6 @@ class Day24 @Inject constructor(
     val yValues = mutableListOf<String>()
     val zValues = mutableListOf<String>()
     val wirings = mutableMapOf<String, Wiring>()
-
-    val swaps = mutableMapOf<String, String>(
-//      /*"kth" to "z12",*/ "carryChain13" to "z12",
-//      /*"z12" to "kth",*/ "z12" to "carryChain13",
-//      /*"gsd" to "z26",*/ "carry26" to "z26",
-//      /*"z26" to "gsd",*/ "z26" to "carry26",
-//      /*"tbt" to "z32",*/ "carryAfter32" to "z32",
-//      /*"z32" to "tbt",*/ "z32" to "carryAfter32",
-//      "qnf" to "vpm",
-//      "vpm" to "qnf",
-    )
 
     input.forEach input@{ line ->
       if (line.isEmpty()) {
@@ -74,33 +56,19 @@ class Day24 @Inject constructor(
           data[key] = { value.toInt() }
         }
       } else {
-        line.split(" ").let { (a, operation, b, _, possibleResult) ->
+        line.split(" ").let { (a, operation, b, _, result) ->
+          if (result.startsWith('z')) { zValues.add(result) }
 
-          val result = swaps[possibleResult] ?: possibleResult
+          val op = findOperation(operation)
 
-          if (result.startsWith('z')) {
-            zValues.add(result)
-          }
-          val op = when (operation) {
-            "AND" -> Int::and
-            "OR" -> Int::or
-            "XOR" -> Int::xor
-            else -> throw IllegalArgumentException("Unknown operation $operation")
-          }
           wirings[result] = Wiring(a, operation, b, result)
-          data[result] = {
-            op(data[a]!!(), data[b]!!())
-          }
+
+          data[result] = { op(data[a]!!(), data[b]!!()) }
         }
       }
     }
 
-    val binaryX = xValues.sortedDescending().map { data[it]!!() }.joinToString("")
-    val binaryY = yValues.sortedDescending().map { data[it]!!() }.joinToString("")
-
-    val goodBinaryZ = (binaryX.toLong(2) + binaryY.toLong(2)).toString(2)
-//    val result222 = zValues.sortedDescending().map { data[it]!!() }.joinToString("")
-//    val goodBinaryZ = (binaryX.toLong(2) + binaryY.toLong(2)).toString(2)
+    val swaps = mutableMapOf<String, String>()
 
     fun swap(swap: Pair<String, String>) {
       val oldA = wirings[swap.first]!!
@@ -124,26 +92,6 @@ class Day24 @Inject constructor(
 
     val correctWirings = mutableMapOf<String, List<Wiring>>()
 
-    fun populateCorrectWirings(result: String) {
-      val myWirings = mutableListOf<Wiring>()
-      val toCheck = LinkedList<String>()
-      toCheck.add(result)
-      while(toCheck.isNotEmpty()) {
-        val wiring = wirings[toCheck.poll()]!!
-        val (a, op, b, r) = wiring
-
-        myWirings.add(wiring)
-
-        val aIsInput = a.startsWith('x') || a.startsWith('y')
-        val bIsInput = b.startsWith('x') || b.startsWith('y')
-
-        if (!aIsInput) { toCheck.add(a) }
-        if (!bIsInput) { toCheck.add(b) }
-      }
-
-      correctWirings[result] = myWirings
-    }
-
     (0 .. 45).forEach loop@{ n ->
       val previousPreviousKey = "z${(n-2).toString().padStart(2, '0')}"
       val previousKey = "z${(n-1).toString().padStart(2, '0')}"
@@ -160,15 +108,18 @@ class Day24 @Inject constructor(
         )?.also { swap(it) }
         // don't need to check the last - the error won't be there.
       }
-      populateCorrectWirings(key)
-    }
-    if (swaps.size != 8) {
-      throw IllegalStateException("Did not find all the swaps")
+      correctWirings[key] = getMyWirings(key, wirings)
     }
 
-    val result = zValues.sortedDescending().map { data[it]!!() }.joinToString("")
-    if (result != goodBinaryZ) throw IllegalStateException("We found eight swaps, but didn't get the right result")
-    
+    if (swaps.size != 8) { throw IllegalStateException("Did not find all the swaps") }
+
+    val binaryX = xValues.sortedDescending().map { data[it]!!() }.joinToString("")
+    val binaryY = yValues.sortedDescending().map { data[it]!!() }.joinToString("")
+    val expectedResult = (binaryX.toLong(2) + binaryY.toLong(2)).toString(2)
+    val actualResult = zValues.sortedDescending().map { data[it]!!() }.joinToString("")
+
+    if (actualResult != expectedResult) { throw IllegalStateException("We found eight swaps, but didn't get the right result") }
+
     swaps.keys.sorted().joinToString(",")
   }
 
@@ -242,21 +193,7 @@ class Day24 @Inject constructor(
     previousPreviousWirings: Pair<Int, String>,
     correctWirings: Map<String, List<Wiring>>,
   ): Pair<String, String>? {
-    val myWirings = mutableListOf<Wiring>()
-    val toCheck = LinkedList<String>()
-    toCheck.add(key.second)
-    while(toCheck.isNotEmpty()) {
-      val wiring = wirings[toCheck.poll()]!!
-      val (a, op, b, r) = wiring
-
-      myWirings.add(wiring)
-
-      val aIsInput = a.startsWith('x') || a.startsWith('y')
-      val bIsInput = b.startsWith('x') || b.startsWith('y')
-
-      if (!aIsInput) { toCheck.add(a) }
-      if (!bIsInput) { toCheck.add(b) }
-    }
+    val myWirings = getMyWirings(key.second, wirings)
 
     val currentWirings = myWirings.minus(correctWirings[previousWirings.second]!!)
     val previousNewWirings = correctWirings[previousWirings.second]!!.minus(correctWirings[previousPreviousWirings.second]!!)
@@ -266,13 +203,8 @@ class Day24 @Inject constructor(
     // sum(N-1)         AND  carryChain(N-1)  = carryAfter(N-1)
     // y(N-1)           AND  x(N-1)           = carry(N-1)
     // carryAfter(N-1)  OR   carry(N-1)       = carryChainN
-    // yN               XOR     yN            = sumN
+    // yN               XOR yN                = sumN
     // sumN             XOR  carryChainN      = zN
-
-    // carry(N-2) is in the previousNewWirings as x(N-2) AND y(N-2)
-    val x2 = previousPreviousWirings.first.let { "x${it.toString().padStart(2, '0')}" }
-    val y2 = previousPreviousWirings.first.let { "y${it.toString().padStart(2, '0')}" }
-    val carryN2 = previousNewWirings.first { w -> w.op == "AND" && setOf(w.a, w.b) == setOf(x2, y2) }
 
     // sum(N-1)
     val x1 = previousWirings.first.let { "x${it.toString().padStart(2, '0')}" }
@@ -291,16 +223,15 @@ class Day24 @Inject constructor(
     val carryChainN1 = previousNewWirings.find { w -> w.op == "OR" } ?: previousNewWirings.first { w -> w.op == "AND" }
     // carryAfter(N-1)
     val carryAfterN1 = wirings.entries.find { (_, w) -> w.op == "AND" && setOf(w.a, w.b) == setOf(sumN1.result, carryChainN1.result) }?.value
-
     if (carryAfterN1 == null) {
-      // TODO: update to match below
+      // doesn't happen on input
       throw IllegalArgumentException("Something Wrong with $carryChainN1 or $sumN1")
     }
 
     // carryChainN
     val carryChainN = wirings.entries.find { (_, w) -> w.op == "OR" && setOf(w.a, w.b) == setOf(carryAfterN1.result, carryN1.result) }?.value
     if (carryChainN == null) {
-      // TODO: update to match below
+      // doesn't happen on input
       throw IllegalArgumentException("Something Wrong with $carryAfterN1 or $carryN1")
     }
 
@@ -308,12 +239,12 @@ class Day24 @Inject constructor(
     val zN = wirings.entries.find { (_, w) -> w.op == "XOR" && setOf(w.a, w.b) == setOf(sumN.result, carryChainN.result) }?.value
     if (zN == null) {
       val correctZN = wirings[key.second]!!
-      if (carryChainN.result in correctZN.input()) {
+      return if (carryChainN.result in correctZN.input()) {
         // something bad with sumN
-        return sumN.result to correctZN.input().minus(carryChainN.result).first()
+        sumN.result to correctZN.input().minus(carryChainN.result).first()
       } else {
         // something bad with carryChainN
-        return carryChainN.result to correctZN.input().minus(sumN.result).first()
+        carryChainN.result to correctZN.input().minus(sumN.result).first()
       }
     }
 
@@ -324,6 +255,26 @@ class Day24 @Inject constructor(
     }
 
     return null
+  }
+
+  private fun getMyWirings(key: String, wirings: MutableMap<String, Wiring>): MutableList<Wiring> {
+    val myWirings = mutableListOf<Wiring>()
+    val toCheck = LinkedList<String>()
+    toCheck.add(key)
+    while(toCheck.isNotEmpty()) {
+      val wiring = wirings[toCheck.poll()]!!
+      val (a, op, b, r) = wiring
+
+      myWirings.add(wiring)
+
+      val aIsInput = a.startsWith('x') || a.startsWith('y')
+      val bIsInput = b.startsWith('x') || b.startsWith('y')
+
+      if (!aIsInput) { toCheck.add(a) }
+      if (!bIsInput) { toCheck.add(b) }
+    }
+
+    return myWirings
   }
 }
 
