158c8d0277d56be76c4e5652ec603a20dca42c01
Brian Peck <peckb@squareup.com>
2025-12-02 10:24:41 -0800

    Upgrade libraries now that Kotlin is upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,17 +26,18 @@ repositories {
 dependencies {
     // main dependencies
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
-    implementation("io.arrow-kt:arrow-core:1.2.4")
+    implementation("io.arrow-kt:arrow-core:2.2.0")
+    implementation("io.arrow-kt:arrow-functions:2.2.0")
     implementation("org.apache.commons:commons-text:1.14.0")
     implementation("org.apache.commons:commons-math3:3.6.1")
     implementation("org.apache.commons:commons-geometry-core:1.0")
     implementation("org.apache.commons:commons-geometry-euclidean:1.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("tools.aqua:z3-turnkey:4.14.1")
     implementation("org.jgrapht:jgrapht-core:1.5.2")
 
     // used for generating skeletons
-    implementation("com.squareup:kotlinpoet:1.18.1")
+    implementation("com.squareup:kotlinpoet:2.2.0")
 
     // dependency injection library and annotation processing
     implementation("com.google.dagger:dagger:2.57.2")
@@ -45,8 +46,8 @@ dependencies {
 
     // test libraries
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
-    testImplementation("org.junit.platform:junit-platform-launcher:1.10.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.1")
+    testImplementation("org.junit.platform:junit-platform-launcher:6.0.1")
 }
 
 tasks.test {
