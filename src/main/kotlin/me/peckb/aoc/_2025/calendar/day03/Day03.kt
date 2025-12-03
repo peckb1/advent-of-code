fe746bc1a867c3b9ee3e3447983b7f891a4356d6
Brian Peck <peckb@squareup.com>
2025-12-02 22:06:37 -0800

    formatting

--- src/main/kotlin/me/peckb/aoc/_2025/calendar/day03/Day03.kt
+++ src/main/kotlin/me/peckb/aoc/_2025/calendar/day03/Day03.kt
@@ -14,21 +14,15 @@ class Day03 @Inject constructor(
     batteryArrays.sumOf { batteryArray -> maxJoltage(batteryArray, numToEnable = 12) }
   }
 
-  private fun maxJoltage(
-    batteryArray: List<Long>,
-    startIndex: Int = 0,
-    numToEnable: Int,
-  ) : Long {
-    val subListIndices = startIndex .. ((batteryArray.size - 1) - (numToEnable - 1))
-
-    val indexOfLargest = subListIndices.maxBy { i -> batteryArray[i] }
-    val largestStartValue = batteryArray[indexOfLargest]
+  private fun maxJoltage(batteryArray: List<Long>, startIndex: Int = 0, numToEnable: Int) : Long {
+    val indicesOfFlippableBatteries = startIndex .. ((batteryArray.size - 1) - (numToEnable - 1))
+    val largestStartValue = indicesOfFlippableBatteries.maxOf { i -> batteryArray[i] }
 
     // we can bottom out if our next recurse would enable zero batteries
     if (numToEnable == 1) { return largestStartValue }
 
     // if we have more batteries to flip - find the indices for those batteries and send down the new arrays
-    val possibleStartIndices = subListIndices.filter { i -> batteryArray[i] == largestStartValue }
+    val possibleStartIndices = indicesOfFlippableBatteries.filter { i -> batteryArray[i] == largestStartValue }
     val maxSubJoltage = possibleStartIndices.maxOf { i -> maxJoltage(batteryArray, startIndex = i + 1, numToEnable = numToEnable - 1) }
 
     // then add it to our value and return!
