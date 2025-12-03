6485b21fe5f6f758beddca97cb0d882be726dbc6
Brian Peck <peckb@squareup.com>
2025-12-02 21:38:40 -0800

    Optimize by not recursing n=0

--- src/main/kotlin/me/peckb/aoc/_2025/calendar/day03/Day03.kt
+++ src/main/kotlin/me/peckb/aoc/_2025/calendar/day03/Day03.kt
@@ -15,28 +15,25 @@ class Day03 @Inject constructor(
   }
 
   private fun maxJoltage(batteryArray: BatteryArray, numToEnable: Int) : Long {
-    if (numToEnable == 0) { return 0 }
-
     val subListIndices = batteryArray.batteries.drop(numToEnable - 1).indices
-
     val indexOfLargestStart = subListIndices.maxBy { i -> batteryArray.batteries[i] }
     val largestStartValue = batteryArray.batteries[indexOfLargestStart]
 
+    // we can bottom out if our next recurse would enable zero batteries
+    if (numToEnable == 1) { return largestStartValue }
+
+    // if we have more batteries to flip - find the indices for those batteries and send down the new arrays
     val possibleStartIndices = subListIndices.filter { i -> batteryArray.batteries[i] == largestStartValue }
+    val maxSubJoltage = possibleStartIndices.maxOf { i -> maxJoltage(batteryArray.dropAfter(i), numToEnable - 1) }
 
-    val subJoltages = possibleStartIndices.maxOf { i -> maxJoltage(batteryArray.dropAfter(i), numToEnable - 1) }
-
-    return if (subJoltages > 0) {
-      "$largestStartValue$subJoltages".toLong()
-    } else {
-      largestStartValue.toLong()
-    }
+    // then add it to our value and return!
+    return "$largestStartValue$maxSubJoltage".toLong()
   }
 
-  private fun battery(line: String) = line.map { it.digitToInt() }.let(::BatteryArray)
+  private fun battery(line: String) = line.map { it.digitToInt().toLong() }.let(::BatteryArray)
 }
 
-data class BatteryArray(val batteries: List<Int>) {
+data class BatteryArray(val batteries: List<Long>) {
   fun dropAfter(index: Int) : BatteryArray {
     return BatteryArray(batteries.drop(index + 1))
   }
