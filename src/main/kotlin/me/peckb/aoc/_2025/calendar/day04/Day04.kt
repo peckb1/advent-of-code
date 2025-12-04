2564b55dcff20472e0583da7ced60c9c68c55d71
Brian Peck <peckb@squareup.com>
2025-12-03 21:42:54 -0800

    more cleanup

--- src/main/kotlin/me/peckb/aoc/_2025/calendar/day04/Day04.kt
+++ src/main/kotlin/me/peckb/aoc/_2025/calendar/day04/Day04.kt
@@ -2,8 +2,6 @@ package me.peckb.aoc._2025.calendar.day04
 
 import javax.inject.Inject
 import me.peckb.aoc.generators.InputGenerator.InputGeneratorFactory
-import kotlin.math.max
-import kotlin.math.min
 import kotlin.text.forEach
 
 class Day04 @Inject constructor(
@@ -12,70 +10,36 @@ class Day04 @Inject constructor(
   fun partOne(filename: String) = generatorFactory.forFile(filename).read { input ->
     val printingDepartment = mapPaper(input)
 
-    var superCount = 0
+    var removable = 0
+
     printingDepartment.indices.forEach { r ->
-      val row = printingDepartment[r]
-
-      row.indices.forEach { c ->
-        var count = 0
-
-        if (printingDepartment[r][c] == '@') {
-          for (rIndex in (max(0, r - 1)..min(row.size - 1, r + 1))) {
-            for (cIndex in (max(0, c - 1)..min(printingDepartment.size - 1, c + 1))) {
-              if (printingDepartment[rIndex][cIndex] == '@' && !(rIndex == r && cIndex == c)) {
-                count++
-              }
-            }
-          }
-
-          if (count < 4) {
-            superCount++
-          }
-        }
+      printingDepartment[r].indices.forEach { c ->
+        if (printingDepartment[r][c] == '@' && printingDepartment.paperNeighbors(r, c) < 4) { removable++ }
       }
     }
 
-    superCount
+    removable
   }
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).read { input ->
     val printingDepartment = mapPaper(input)
 
-    var superDuperCount = 0
-    var someWereRemoved = true
-    while(someWereRemoved) {
-      var superCount = 0
+    var totalRemovedCount = 0
+
+    do {
       val toRemove = mutableListOf<Pair<Int, Int>>()
+
       printingDepartment.indices.forEach { r ->
-        val row = printingDepartment[r]
-
-
-        row.indices.forEach { c ->
-          var count = 0
-
-          if (printingDepartment[r][c] == '@') {
-            for (rIndex in (max(0, r - 1)..min(row.size - 1, r + 1))) {
-              for (cIndex in (max(0, c - 1)..min(printingDepartment.size - 1, c + 1))) {
-                if (printingDepartment[rIndex][cIndex] == '@' && !(rIndex == r && cIndex == c)) {
-                  count++
-                }
-              }
-            }
-
-            if (count < 4) {
-              toRemove.add(r to c)
-              superCount++
-            }
-          }
+        printingDepartment[r].indices.forEach { c ->
+          if (printingDepartment[r][c] == '@' && printingDepartment.paperNeighbors(r, c) < 4) { toRemove.add(r to c) }
         }
       }
 
       toRemove.forEach { (r, c) -> printingDepartment[r][c] = '.' }
-      someWereRemoved = superCount != 0
-      superDuperCount += superCount
-    }
+      totalRemovedCount += toRemove.size
+    } while (toRemove.isNotEmpty())
 
-    superDuperCount
+    totalRemovedCount
   }
 
   private fun mapPaper(input: Sequence<String>): MutableList<MutableList<Char>> {
@@ -88,5 +52,23 @@ class Day04 @Inject constructor(
     }
 
     return workshop
+  }
+
+  private fun MutableList<MutableList<Char>>.paperNeighbors(r: Int, c: Int): Int {
+    return (-1 .. 1).sumOf { rDelta ->
+      // ensure row bounds
+      val rIndex = rDelta + r
+      if (rIndex !in 0 until size) return@sumOf 0
+
+      (-1 .. 1).count { cDelta ->
+        // don't count ourselves
+        if (rDelta == 0 && cDelta == 0) return@count false
+        // ensure column bounds
+        val cIndex = cDelta + c
+        if (cIndex !in 0 until this[rIndex].size) return@count false
+
+        this[rIndex][cIndex] == '@'
+      }
+    }
   }
 }
