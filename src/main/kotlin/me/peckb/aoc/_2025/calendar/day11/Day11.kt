dd04255a45be5f9b5119484a2ac771f70dc57393
Brian Peck <peckb@squareup.com>
2025-12-10 23:16:19 -0800

    associateBy

--- src/main/kotlin/me/peckb/aoc/_2025/calendar/day11/Day11.kt
+++ src/main/kotlin/me/peckb/aoc/_2025/calendar/day11/Day11.kt
@@ -7,13 +7,13 @@ class Day11 @Inject constructor(
   private val generatorFactory: InputGeneratorFactory,
 ) {
   fun partOne(filename: String) = generatorFactory.forFile(filename).readAs(::server) { input ->
-    val serverMap = input.plus(Server("out")).map { it.id to it }.toMap()
+    val serverMap = input.plus(Server("out")).associateBy { it.id }
 
     countAllPaths(serverMap, serverMap["you"]!!)
   }
 
   fun partTwo(filename: String) = generatorFactory.forFile(filename).readAs(::server) { input ->
-    val serverMap = input.plus(Server("out")).map { it.id to it }.toMap()
+    val serverMap = input.plus(Server("out")).associateBy { it.id }
 
     countAllPaths(
       serverMap = serverMap,
